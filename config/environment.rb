5627764204efde4ba31c28a1856a51568c7cb57d
breisinger <breisinger@4e22fdd3-d838-0410-b171-8f2b50891703>
2012-10-18 20:12:04 +0000

    GOVD-3553 logging again

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,10 @@
 # set up logging
-# require File.expand_path("../logging", __FILE__)
+require File.expand_path("../logging", __FILE__)
 
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+
+Rails.logger = Log4r::Logger['default']
 
 # Initialize the rails application
 Tsms::Application.initialize!
--- config/logging.rb
+++ config/logging.rb
@@ -1,7 +1,8 @@
 require 'log4r/yamlconfigurator'
 require 'log4r/outputter/datefileoutputter'
+require 'log4r/formatter/patternformatter'
+
 config = Log4r::YamlConfigurator
-config['FILE_NAME'] = "tsms_#{Rails.env}.log"
-config['LOG_DIR']   = Rails.root.join("log").to_s
-config.load_yaml_file(Rails.root.join("config", "log4r.yml"))
-Rails.logger = Log4r::Logger['default']
+config['FILE_NAME'] = "tsms_#{ENV['RAILS_ENV'] || 'development'}.log"
+config['LOG_DIR']   = File.expand_path("../../log", __FILE__)
+config.load_yaml_file(File.expand_path("../log4r.yml", __FILE__))
