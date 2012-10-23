5d0ce04bcf7ce1ab12a26c93211c78e0b389417e
bortega <bortega@4e22fdd3-d838-0410-b171-8f2b50891703>
2012-10-23 15:54:15 +0000

    prevent duplicate vendor records (e.g. so running rake db:seed is idempotent)

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -2,7 +2,8 @@ class Vendor < ActiveRecord::Base
   attr_accessible :name, :username, :password, :from, :worker
 
   has_many :accounts
-  
+
   validates_presence_of [:name, :username, :password, :from, :worker]
+  validates_uniqueness_of :name
   validates_length_of [:name, :username, :password, :from, :worker], :maximum => 256
 end
