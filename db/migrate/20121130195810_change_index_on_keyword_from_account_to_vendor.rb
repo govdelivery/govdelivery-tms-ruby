563a17bbff1dbd746e7017ccfc763b6d54dfeb29
tholien <tholien@4e22fdd3-d838-0410-b171-8f2b50891703>
2012-12-06 16:20:37 +0000

    More migration madness.
    Part of this migration ran in stage before I swapped the order of create
    event handler and this one, so the index it removes was already removed.
    
    I really hope this is the last one. Sorry.

--- db/migrate/20121130195810_change_index_on_keyword_from_account_to_vendor.rb
+++ db/migrate/20121130195810_change_index_on_keyword_from_account_to_vendor.rb
@@ -1,11 +1,17 @@
 class ChangeIndexOnKeywordFromAccountToVendor < ActiveRecord::Migration
   def up
-    remove_index :keywords, [:account_id, :name]
+    safely { remove_index :keywords, [:account_id, :name] }
     add_index :keywords, [:vendor_id, :name], :unique => true
   end
 
   def down
     add_index :keywords, [:account_id, :name], :unique => true
     remove_index :keywords, [:vendor_id, :name]
+  end
+
+  def safely
+    yield
+  rescue Exception => e
+    say "exception ignored: #{e}"
   end
 end
