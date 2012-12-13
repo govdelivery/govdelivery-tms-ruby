c43f1010286f201be7256423863403a676823abd
breisinger <breisinger@4e22fdd3-d838-0410-b171-8f2b50891703>
2012-12-13 17:07:40 +0000

    XACT-3 use email_subscribe dcm action when appropriate

--- app/models/dcm_subscribe_action.rb
+++ app/models/dcm_subscribe_action.rb
@@ -1,14 +1,34 @@
+# Sends a subscription action to DCM
+#
+# Example: call("1+2222222222", "ACME:TOPIC_1,TOPIC2", ['foo@bar.com']
+#   -> Subscribe foo@bar.com to ACME topics TOPIC_1 and TOPIC_2
+#   In this scenario, the user text was something like "subscribe foo@bar.com"
+#
+# Example: call("1+2222222222", "ACME:TOPIC_1,TOPIC2", [])
+#   -> Subscribe 1+2222222222 to ACME topics TOPIC_1 and TOPIC_2 
+#   In this scenario, the user text was something like "subscribe"
+#
 DcmSubscribeAction = Struct.new(:client) do
-  # phone_number: a non-formatted number
+  # phone_number: the incoming phone number string
   # data_string: the params from the subscribe action in the database
-  # subscribe_args: the tokens used after the configured keyword. For example, 
-  #   if the user texted "subscribe foo bar", the subscribe_args should be ['foo', 'bar']
+  # subscribe_args: an array of the tokens the user typed after the configured keyword. For example, 
+  #   if the user texted "subscribe foo@bar.com", the subscribe_args should be ["foo@bar.com"].
   def call(phone_number, data_string, subscribe_args=[], phone_number_constructor=PhoneNumber.public_method(:new))
-    pn = phone_number_constructor.call(phone_number)
-    client.wireless_subscribe(pn.dcm, *parse(data_string))
+    if(email_address = extract_email(subscribe_args))
+      client.email_subscribe(email_address, *parse(data_string))
+    else
+      pn = phone_number_constructor.call(phone_number)
+      client.wireless_subscribe(pn.dcm, *parse(data_string))
+    end
   end
 
   private
+
+  def extract_email(subscribe_args)
+    if !subscribe_args[0].nil? && subscribe_args[0] =~ /@/
+      subscribe_args[0]
+    end
+  end
 
   # account_code:topic1,topic2,topic3
   def parse(str)
--- spec/models/dcm_subscribe_action_spec.rb
+++ spec/models/dcm_subscribe_action_spec.rb
@@ -1,5 +1,6 @@
-require_relative '../../app/models/dcm_subscribe_action'
-require_relative '../little_spec_helper'
+require File.expand_path('../../../app/models/phone_number', __FILE__)
+require File.expand_path('../../../app/models/dcm_subscribe_action', __FILE__)
+require File.expand_path('../../little_spec_helper', __FILE__)
 
 describe DcmSubscribeAction do
   let(:client) { mock('dcm_client') }
@@ -9,7 +10,20 @@ describe DcmSubscribeAction do
   it 'should call wireless_subscribe on the DCM Client' do
     client.expects(:wireless_subscribe).with('1+4443332222', 'ACCOUNT_CODE', ['TOPIC_CODE', 'TOPIC_2'])
 
-    subject.call('+14443332222', 'ACCOUNT_CODE:TOPIC_CODE,TOPIC_2', fake_phone_number_constructor)
+    subject.call('+14443332222', 'ACCOUNT_CODE:TOPIC_CODE,TOPIC_2', [], fake_phone_number_constructor)
+  end
+
+  context "with subscribe args" do
+    it 'should call email_subscribe on the DCM Client when email is valid' do
+      client.expects(:email_subscribe).with('donkey@govdelivery.com', 'ACCOUNT_CODE', ['TOPIC_CODE', 'TOPIC_2'])
+
+      subject.call('+14443332222', 'ACCOUNT_CODE:TOPIC_CODE,TOPIC_2', ['donkey@govdelivery.com'], fake_phone_number_constructor)
+    end
+    it 'should call email_subscribe on the DCM Client when email is invalid' do
+      client.expects(:wireless_subscribe).with('1+4443332222', 'ACCOUNT_CODE', ['TOPIC_CODE', 'TOPIC_2'])
+
+      subject.call('+14443332222', 'ACCOUNT_CODE:TOPIC_CODE,TOPIC_2', ['donkeygovdelivery.com'], fake_phone_number_constructor)
+    end
   end
 
   it 'does not rescue exceptions' do
@@ -17,7 +31,7 @@ describe DcmSubscribeAction do
     client.expects(:wireless_subscribe).raises(error)
 
     begin
-      subject.call('+14443332222', 'ACCOUNT_CODE:TOPIC_CODE,TOPIC_2', fake_phone_number_constructor)
+      subject.call('+14443332222', 'ACCOUNT_CODE:TOPIC_CODE,TOPIC_2', [], fake_phone_number_constructor)
     rescue Exception => e
       e.should == error
     end
