780a61476c0298bf1a7d3977e0c51f971d513a8d
bortega <bortega@4e22fdd3-d838-0410-b171-8f2b50891703>
2013-01-14 20:51:24 +0000

    remove MessageSender

--- app/models/message_sender.rb
+++
@@ -1,8 +0,0 @@
-MessageSender = Struct.new(:from) do
-  def send!(recipients, send_proc)
-    recipients.each do |r|
-      result = send_proc.call(from, r.formatted_phone)
-      r.complete!(result[:status], result[:ack], result[:error])
-    end
-  end
-end
