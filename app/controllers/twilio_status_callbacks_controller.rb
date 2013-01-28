a603b385aae454ec49c5f7151eef2691c64e7343
rwatkins <rwatkins@4e22fdd3-d838-0410-b171-8f2b50891703>
2013-01-28 17:42:03 +0000

    XACT-67 Combine voice and sms callbacks from Twilio

--- app/controllers/twilio_status_callbacks_controller.rb
+++ app/controllers/twilio_status_callbacks_controller.rb
@@ -1,22 +1,40 @@
 class TwilioStatusCallbacksController < ApplicationController
   skip_before_filter :authenticate_user!
-  before_filter :find_recipient
+  before_filter :get_status, :find_recipient
   respond_to :xml
 
   def create
-    status = case params['SmsStatus']
-               when 'sent'
+    status = case @status
+               when 'sent', 'completed', 'busy', 'no-answer'
                  RecipientStatus::SENT
                when 'failed'
                  RecipientStatus::FAILED
+               when 'canceled'
+                 RecipientStatus::CANCELED
              end
     @recipient.complete!(:status => status)
     render :text => '', :status => 201
   end
 
   protected
+  def get_status
+    @status = if params.has_key?('SmsStatus')
+                params['SmsStatus']
+              elsif params.has_key?('CallStatus')
+                params['CallStatus']
+              else
+                ''
+              end
+  end
+
   def find_recipient
-    @recipient=SmsRecipient.find_by_ack!(params['SmsSid'])
+    @recipient=if params.has_key?('SmsStatus')
+                 SmsRecipient.find_by_ack!(params['SmsSid'])
+               elsif params.has_key?('CallStatus')
+                 VoiceRecipient.find_by_ack!(params['CallSid'])
+               else
+                 nil
+               end
   end
 
 end
