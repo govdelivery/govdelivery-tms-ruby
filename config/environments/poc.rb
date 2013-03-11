b5d06a02043ef221d2290338c3be9e443aa548a1
ben <ben@4e22fdd3-d838-0410-b171-8f2b50891703>
2013-03-11 20:13:11 +0000

    TOPS-1234 update to EP redis.

--- config/environments/poc.rb
+++ config/environments/poc.rb
@@ -41,7 +41,7 @@ Xact::Application.configure do
 
   # Use a different cache store in production
 
-  config.redis_url = 'redis://poc-redis-master.visi.gdi:6379'
+  config.redis_url = 'redis://poc-redis1.tops.gdi:6379'
   config.cache_store = :redis_store, config.redis_url
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
