f4c96b75c3c6a4caf34e0d81908a98345c7ac2f8
bortega <bortega@4e22fdd3-d838-0410-b171-8f2b50891703>
2013-03-15 17:11:57 +0000

    XACT-100 okay but really don't do ODM polling

--- app/workers/scheduler/odm.rb
+++ app/workers/scheduler/odm.rb
@@ -1,22 +1,24 @@
-module Scheduler
-  class ScheduleTmsStats < TrinidadScheduler.Cron Rails.configuration.odm_stats_crontab
-    def run
-      _logger.info("Running #{self.class.name} at #{Time.zone.now}")
-      Odm::TmsExtendedStatisticsWorker.perform_async
+if Rails.configuration.odm_polling_enabled && $servlet_context
+  module Scheduler
+    class ScheduleTmsStats < TrinidadScheduler.Cron Rails.configuration.odm_stats_crontab
+      def run
+        _logger.info("Running #{self.class.name} at #{Time.zone.now}")
+        Odm::TmsExtendedStatisticsWorker.perform_async
+      end
     end
-  end
 
-  class ScheduleTmsOpenStats < TrinidadScheduler.Cron Rails.configuration.odm_opens_crontab
-    def run
-      _logger.info("Running #{self.class.name} at #{Time.zone.now}")
-      Odm::TmsExtendedOpensWorker.perform_async
+    class ScheduleTmsOpenStats < TrinidadScheduler.Cron Rails.configuration.odm_opens_crontab
+      def run
+        _logger.info("Running #{self.class.name} at #{Time.zone.now}")
+        Odm::TmsExtendedOpensWorker.perform_async
+      end
     end
-  end
 
-  class ScheduleTmsClickStats < TrinidadScheduler.Cron Rails.configuration.odm_clicks_crontab
-    def run
-      _logger.info("Running #{self.class.name} at #{Time.zone.now}")
-      Odm::TmsExtendedClicksWorker.perform_async
+    class ScheduleTmsClickStats < TrinidadScheduler.Cron Rails.configuration.odm_clicks_crontab
+      def run
+        _logger.info("Running #{self.class.name} at #{Time.zone.now}")
+        Odm::TmsExtendedClicksWorker.perform_async
+      end
     end
   end
-end if $servlet_context
+end
--- app/workers/scheduler/twilio.rb
+++ app/workers/scheduler/twilio.rb
@@ -1,16 +1,18 @@
-module Scheduler
-  class ScheduleTwilioSmsPoll < TrinidadScheduler.Cron Rails.configuration.twilio_sms_poll_crontab
-    def run
-      _logger.info("Running #{self.class.name} at #{Time.zone.now}")
-      Twilio::VoicePollingWorker.perform_async
+if Rails.configuration.twilio.polling_enabled && $servlet_context
+  module Scheduler
+    class ScheduleTwilioSmsPoll < TrinidadScheduler.Cron Rails.configuration.twilio_sms_poll_crontab
+      def run
+        _logger.info("Running #{self.class.name} at #{Time.zone.now}")
+        Twilio::VoicePollingWorker.perform_async
+      end
     end
-  end
 
-  class ScheduleTwilioVoicePoll < TrinidadScheduler.Cron Rails.configuration.twilio_voice_poll_crontab
-    def run
-      _logger.info("Running #{self.class.name} at #{Time.zone.now}")
-      Twilio::SmsPollingWorker.perform_async
+    class ScheduleTwilioVoicePoll < TrinidadScheduler.Cron Rails.configuration.twilio_voice_poll_crontab
+      def run
+        _logger.info("Running #{self.class.name} at #{Time.zone.now}")
+        Twilio::SmsPollingWorker.perform_async
+      end
     end
   end
-end if $servlet_context
+end
 
