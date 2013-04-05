afca358c72abb7a383f05f4db910a660b7776a07
bortega <bortega@4e22fdd3-d838-0410-b171-8f2b50891703>
2013-04-05 18:56:46 +0000

    XACT-110 enable TMS polling

--- config/environments/stage.rb
+++ config/environments/stage.rb
@@ -80,7 +80,7 @@ Xact::Application.configure do
     api_root: 'https://stage-api.govdelivery.com' 
   }
 
-  config.odm_polling_enabled = false
+  config.odm_polling_enabled = true
   config.odm_host = "http://stg-odm1-ep.tops.gdi:65080"
   config.odm_endpoint = "#{config.odm_host}/service/TMSExtended"
   config.odm_username = 'xact'
