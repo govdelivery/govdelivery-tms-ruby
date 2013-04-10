05d3575a44649f42cc490e179d648a30a7435927
bortega <bortega@4e22fdd3-d838-0410-b171-8f2b50891703>
2013-04-10 21:47:23 +0000

    XACT-109 check messages for completion more frequently in CI builds

--- config/environments/ci.rb
+++ config/environments/ci.rb
@@ -47,4 +47,6 @@ Xact::Application.configure do
   # a SMS Message.  We don't want to send a callback_url parameter when the application
   # is not accessible from the internet.
   config.public_callback = false
+
+  config.message_completion_crontab = "*/10 * * * * ?"
 end
