2d6383ebc385145118200cf92ed59acbbb325219
breisinger <breisinger@4e22fdd3-d838-0410-b171-8f2b50891703>
2013-05-22 15:12:30 +0000

    XACT-119 make services list easier to parse

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -2,21 +2,21 @@ class ServicesController < ApplicationController
   before_filter :find_user
 
   def index
-    @services = [{ :self => root_path }]
+    @services = { :self => root_path }
 
     if @account.sms_vendor
-      @services << {:keywords => keywords_path}
-      @services << {:command_types => command_types_path }
-      @services << {:inbound_sms_messages => inbound_sms_index_path }
-      @services << {:sms_messages => sms_index_path }
+      @services[:keywords] = keywords_path
+      @services[:command_types] = command_types_path 
+      @services[:inbound_sms_messages] = inbound_sms_index_path 
+      @services[:sms_messages] = sms_index_path 
     end
 
     if @account.email_vendor
-      @services << {:email_messages => email_index_path}
+      @services[:email_messages] = email_index_path
     end
 
     if @account.voice_vendor
-      @services << {:voice_messages => voice_index_path}
+      @services[:voice_messages] = voice_index_path
     end
   end
 end
