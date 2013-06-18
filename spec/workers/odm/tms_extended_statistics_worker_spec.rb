4708bb3d15c3e12f6ceed5335cde6f7d60e37a4b
breisinger <breisinger@4e22fdd3-d838-0410-b171-8f2b50891703>
2013-06-18 16:55:51 +0000

    XACT-126 even though it's strange that we got data from another user back from ODM, ignore it without failing.

--- spec/workers/odm/tms_extended_statistics_worker_spec.rb
+++ spec/workers/odm/tms_extended_statistics_worker_spec.rb
@@ -2,6 +2,16 @@ require 'spec_helper'
 if defined?(JRUBY_VERSION)
 
   describe Odm::TmsExtendedStatisticsWorker do
+    let(:worker) { Odm::TmsExtendedStatisticsWorker.new }
+    let(:service_stub) { stub(:delivery_events => delivery_events)}
+    let(:delivery_events) { [stub(:recipient_id => "not_numeric", :message_id => "foo", :address => "hey@bob.evotest.com")] }
 
+    before do
+      worker.service = service_stub
+    end
+
+    it 'should handle integer cast exceptions' do
+      expect { worker.process_vendor(nil) }.to_not raise_error
+    end
   end
 end
