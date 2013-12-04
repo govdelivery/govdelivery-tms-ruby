f6d578d8a824d78fa22a4021d74ccd4a6175ed07
Billy Reisinger <billy.reisinger@govdelivery.com>
2013-12-04 14:34:34 -0600

    XACT-201 command_action API should not puke when displaying stop command actions

--- app/views/command_actions/show.rabl
+++ app/views/command_actions/show.rabl
@@ -2,7 +2,15 @@ object @command_action
 attributes :status, :content_type, :response_body, :created_at
 
 node(:_links) do |m|
-  {:self => inbound_sms_command_action_path(m.inbound_message_id, m.id),
-   :inbound_sms_message => inbound_sms_path(m.inbound_message_id),
-   :command => keyword_command_path(m.command.keyword.id, m.command_id)}
+  result = {
+    :self => inbound_sms_command_action_path(m.inbound_message_id, m.id),
+    :inbound_sms_message => inbound_sms_path(m.inbound_message_id)
+  }
+
+  # stop and help commands don't have keywords and aren't accessible via the API
+  unless m.command.keyword.nil?
+    result[:command] = keyword_command_path(m.command.keyword.id, m.command_id)
+  end
+
+  result
 end
--- spec/views/command_actions/index.rabl_spec.rb
+++ spec/views/command_actions/index.rabl_spec.rb
@@ -15,15 +15,45 @@ describe 'command_actions/index.rabl' do
       )
     end
   end
-
-  before do
-    assign(:command_actions, command_actions)
-    controller.stubs(:url_options).returns(:host => "test.host", :protocol => "http://", :_path_segments => {:action => "show", :controller => "command_actions", :sms_id => 2}, :script_name => "")
-    render
-    @json = ActiveSupport::JSON.decode(rendered)
+  
+  # No keyword id on these
+  let(:stop_command_actions) do
+    5.times.map do |i|
+      stub('CommandAction',
+           id: 1,
+           inbound_message_id: 2,
+           command_id: 1,
+           command: stub(keyword_id: nil, keyword: nil),
+           response_body: 'http body',
+           status: 200,
+           content_type: 'text/plain',
+           created_at: Time.now.beginning_of_day
+      )
+    end
   end
-  it 'should have one item' do
-    rendered.should have_json_type(Array)
-    rendered.should have_json_size(5)
+
+  context "with keywords" do
+    before do
+      assign(:command_actions, command_actions)
+      controller.stubs(:url_options).returns(:host => "test.host", :protocol => "http://", :_path_segments => {:action => "show", :controller => "command_actions", :sms_id => 2}, :script_name => "")
+      render
+      @json = ActiveSupport::JSON.decode(rendered)
+    end
+    it 'should have one item' do
+      rendered.should have_json_type(Array)
+      rendered.should have_json_size(5)
+    end
+  end
+  context "for stop/help commands" do
+    before do
+      assign(:command_actions, stop_command_actions)
+      controller.stubs(:url_options).returns(:host => "test.host", :protocol => "http://", :_path_segments => {:action => "show", :controller => "command_actions", :sms_id => 2}, :script_name => "")
+      render
+      @json = ActiveSupport::JSON.decode(rendered)
+    end
+    it 'should have one item' do
+      rendered.should have_json_type(Array)
+      rendered.should have_json_size(5)
+    end
   end
 end
