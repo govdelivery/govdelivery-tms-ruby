4e0d3a7e90f3ce3f9216927ccb6de94452cb80e3
Benjamin Ortega <ben.ortega@gmail.com>
2014-02-07 16:03:02 -0600

    make this add_index safe(r)

--- db/migrate/20140116212609_add_index_to_email_messages.rb
+++ db/migrate/20140116212609_add_index_to_email_messages.rb
@@ -7,10 +7,14 @@ class AddIndexToEmailMessages < ActiveRecord::Migration
     # If you aren't running enterprise edition, you get ORA-00439
     # because online index building is an enterprise feature.
     online = ['development', 'test'].include?(Rails.env) ? '' : 'ONLINE'
-    
-    add_index :email_messages, 
-              [:user_id, :created_at, :status, :subject, :id], 
-              name: 'em_idx3',
+
+    begin
+    add_index :email_messages,
+              [:user_id, :created_at, :status, :subject, :id],
+              name: 'em_idx4',
               options: online
+    rescue => e
+      puts "Something went wrong: #{e}"
+    end
   end
 end
