7471a74b528db8c85c0e5485de4a29ccea998016
Ed Lebert <ed.lebert@govdelivery.com>
2014-03-19 14:25:58 -0500

    Added default attr_encrypted key to initializer

--- app/models/ipaws/vendor.rb
+++ app/models/ipaws/vendor.rb
@@ -7,10 +7,8 @@ module IPAWS
 
     validates :cog_id, :user_id, :public_password_encrypted, :private_password_encrypted, :jks, presence: true 
 
-    ENCRYPTION_KEY = ENV['ENCRYPTION_KEY'] || "10536b708d56b7219a0fae56c33a5ea77615d212cc75864885ca52cc9051d2062b2cf3f4b79319864cfba503c3f278ba918a5c82c34e599b84d4e9f394ec99a7"
-
-    attr_encrypted :public_password, key: ENCRYPTION_KEY, attribute: :public_password_encrypted
-    attr_encrypted :private_password, key: ENCRYPTION_KEY, attribute: :private_password_encrypted
+    attr_encrypted :public_password, attribute: :public_password_encrypted
+    attr_encrypted :private_password, attribute: :private_password_encrypted
 
     def client(reload=false)
       @client = nil if reload
