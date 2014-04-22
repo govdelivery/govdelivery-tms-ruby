981341221da48666a8a7a056fb6521659a969767
Tyler Holien <tyler.holien@govdelivery.com>
2014-04-22 10:10:39 -0500

    Explain why calling ThrottledTmsExtendedSenderWorker#perform is an error
    (cherry picked from commit 3ecba232a88bbb18e944d62e29a742a193997999)

--- app/workers/odm/throttled_tms_extended_sender_worker.rb
+++ app/workers/odm/throttled_tms_extended_sender_worker.rb
@@ -2,14 +2,15 @@ require 'base'
 
 module Odm
   class ThrottledTmsExtendedSenderWorker < Odm::TmsExtendedSenderWorker
-    sidekiq_options queue: 'cms_throttled'
-
     def perform(options)
-      Rails.logger.warn('something is processing throttled jobs!!!')
+      # Nothing should be processing jobs out of the cms_throttled queue. It's
+      # just a holding queue for the CmsThrottledWorker to pull from.
+      Rails.logger.error('something is processing throttled jobs!!!')
     end
 
     def self.client_push(item)
-      Sidekiq::Client.push(item.merge('class' => self.superclass, 'queue' => 'cms_throttled').stringify_keys)
+      normalized_item = item.merge('class' => self.superclass, 'queue' => 'cms_throttled').stringify_keys
+      Sidekiq::Client.push(normalized_item)
     end
   end
 end
