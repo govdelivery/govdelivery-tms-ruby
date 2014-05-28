f35c8d228a9a13ddeed384e5e379ad3d377015dc
Chris Thompson <chris.thompson@govdelivery.com>
2014-05-28 09:51:20 -0500

    XACT-291 data migration patch

--- db/migrate/20140527182053_data_migration_create_special_keywords.rb
+++ db/migrate/20140527182053_data_migration_create_special_keywords.rb
@@ -1,15 +1,16 @@
 class DataMigrationCreateSpecialKeywords < ActiveRecord::Migration
   def up
-    exec_insert_account_keywords
-    exec_insert_vendor_keywords
+    username = Rails.configuration.database_configuration[ Rails.env ]["username"]
+    exec_insert_account_keywords(username)
+    exec_insert_vendor_keywords(username)
   end
 
   # these are cartesian product queries which feed the insert command
   # so that any special keywords that should exist but don't get inserted
   # - Jesse
-  def exec_insert_account_keywords
-    execute "insert into person_xdev.keywords
-  select person_xdev.keywords_seq.nextval,
+  def exec_insert_account_keywords username
+    execute "insert into #{username}.keywords
+  select #{username}.keywords_seq.nextval,
     ACCOUNT_ID,
     NAME,
     sys_extract_utc(systimestamp),
@@ -24,18 +25,18 @@ class DataMigrationCreateSpecialKeywords < ActiveRecord::Migration
                   select 'Keywords::AccountHelp' as name from dual
                    union all
                   select 'Keywords::AccountDefault' as name from dual),
-         (select id as vendor_id from person_xdev.sms_vendors),
-         (select id as account_id from person_xdev.accounts)) x
+         (select id as vendor_id from #{username}.sms_vendors),
+         (select id as account_id from #{username}.accounts)) x
   where not exists (select 1
-                      from person_xdev.keywords k
+                      from #{username}.keywords k
                      where x.name = k.name
                        and x.vendor_id = k.vendor_id
                        and x.account_id = k.account_id)"
   end
 
-  def exec_insert_vendor_keywords
-    execute "insert into person_xdev.keywords
-  select person_xdev.keywords_seq.nextval,
+  def exec_insert_vendor_keywords(username)
+    execute "insert into #{username}.keywords
+  select #{username}.keywords_seq.nextval,
     null,
     NAME,
     sys_extract_utc(systimestamp),
@@ -50,9 +51,9 @@ class DataMigrationCreateSpecialKeywords < ActiveRecord::Migration
                   select 'Keywords::VendorHelp' as name from dual
                    union all
                   select 'Keywords::VendorDefault' as name from dual),
-         (select id from person_xdev.sms_vendors)) x
+         (select id from #{username}.sms_vendors)) x
   where not exists (select 1
-                      from person_xdev.keywords k
+                      from #{username}.keywords k
                      where x.name = k.name
                        and x.id = k.vendor_id)"
   end
