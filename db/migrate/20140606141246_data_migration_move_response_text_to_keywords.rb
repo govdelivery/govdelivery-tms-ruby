add6501d34b9560326d41ab17de706027eb91804
Chris Thompson <chris.thompson@govdelivery.com>
2014-06-09 17:31:12 -0500

    XACT-292 fix data migrations

--- db/migrate/20140606141246_data_migration_move_response_text_to_keywords.rb
+++ db/migrate/20140606141246_data_migration_move_response_text_to_keywords.rb
@@ -1,15 +1,28 @@
 class DataMigrationMoveResponseTextToKeywords < ActiveRecord::Migration
-  # conditionals for future proofing
   def up
     Account.all.each do |account|
-      account.stop_keyword.update_attribute :response_text, account.read_attribute(:stop_text) if account.respond_to?(:stop_keyword) && account.stop_keyword.present?
-      account.help_keyword.update_attribute :response_text, account.read_attribute(:help_text) if account.respond_to?(:help_keyword) && account.help_keyword.present?
-      account.default_keyword.update_attribute :response_text, account.read_attribute(:help_text) if account.respond_to?(:default_keyword) && account.default_keyword.present?
+      account.reload #to ensure special keywords exist
+      account.save!
+      account.stop_keyword.update_attribute :response_text, account.read_attribute(:stop_text)
+      account.help_keyword.update_attribute :response_text, account.read_attribute(:help_text)
+      account.default_keyword.update_attribute :response_text, account.read_attribute(:help_text)
+      account.stop_keyword.commands << account.stop_handler.commands
     end
     SmsVendor.all.each do |sms_vendor|
-      sms_vendor.stop_keyword.update_attribute :response_text, sms_vendor.read_attribute(:stop_text) if sms_vendor.respond_to?(:stop_keyword) && sms_vendor.stop_keyword.present?
-      sms_vendor.help_keyword.update_attribute :response_text, sms_vendor.read_attribute(:help_text) if sms_vendor.respond_to?(:help_keyword)  && sms_vendor.help_keyword.present?
-      sms_vendor.default_keyword.update_attribute :response_text, sms_vendor.read_attribute(:help_text) if sms_vendor.respond_to?(:default_keyword)  && sms_vendor.default_keyword.present?
+      sms_vendor.reload #to ensure special keywords exist
+      sms_vendor.save!
+      sms_vendor.stop_keyword.update_attribute :response_text, sms_vendor.read_attribute(:stop_text)
+      sms_vendor.help_keyword.update_attribute :response_text, sms_vendor.read_attribute(:help_text)
+      sms_vendor.default_keyword.update_attribute :response_text, sms_vendor.read_attribute(:help_text)
+      # start doesn't exist yet
+      # there are no commands to be moved to these keywords
+    end
+    #custom commands
+    Command.all.each do |command|
+      keyword = command.event_handler.keyword
+      next unless keyword and puts " #{command.name}"
+      puts "#{command.name} => #{keyword.name}"
+      keyword.commands << command
     end
   end
 
