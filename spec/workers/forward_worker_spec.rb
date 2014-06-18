4d22200799ced08be58462685a5fc3e5079c97d1
Chris Thompson <chris.thompson@govdelivery.com>
2014-06-18 15:35:59 -0500

    XACT-310 cleanup spec forward worker

--- spec/workers/forward_worker_spec.rb
+++ spec/workers/forward_worker_spec.rb
@@ -1,7 +1,8 @@
-require File.expand_path("../../little_spec_helper", __FILE__)
 require 'spec_helper'
 
 describe ForwardWorker do
+  let(:account){ create(:account_with_sms) }
+  let(:command){ create(:forward_command, keyword: account.default_keyword) }
   let(:options) { {url: "url",
                    http_method: "post",
                    username: nil,
@@ -9,20 +10,18 @@ describe ForwardWorker do
                    from: "333",
                    sms_body: "sms body",
                    sms_tokens: ["sms", "body", "tokens"],
-                   account_id: 1,
-                   command_id: 1,
+                   account_id: account.id,
+                   command_id: command.id,
+                   inbound_message_id: create(:inbound_message),
                    callback_url: "http://localhost",
                    sms_body_param_name: "sms_body_d",
                    from_param_name: "from_param"} }
 
   it 'creates one Twilio::SenderWorker job if command.process_response returns a message' do
-    pending("whatever I'll fix this bs later")
-    Service::ForwardService.any_instance.expects(:send)
-    Twilio::SenderWorker.expects(:do_deliver).once
-    fake_command = build(:forward_command).tap{ |c|  c.expects(:process_response).returns( build(:sms_message).tap{ |m| m.expects(:sending!) }) }
-    subject.expects( :command ).returns(fake_command)
-    subject.expects( :account ).returns(stub('account'))
-
+    Service::TwilioMessageService.expects(:deliver!).with( kind_of(SmsMessage), options[:callback_url] )
+    command.expects(:process_response).returns( SmsMessage.new )
+    subject.expects(:command).returns(command)
+    # this is a pretty worthless test. The super calls in :perform and :process_response are getting in the way of stubbing them.
     subject.perform( options )
   end
 
