5a877f6a3a695d407ab74ce4f2c6a26aa8c868a4
Andrew Stucki <andrew.stucki@govdelivery.com>
2014-08-08 12:07:44 -0500

    XACT-330 Rails 4 upgrade seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,35 +11,41 @@
 #   rake db:seed
 ##
 
-twilio_sms_sender = SmsVendor.find_or_create_by_name!(:name => 'Twilio Sender',
-                                                      :worker => 'TwilioMessageWorker',
-                                                      :username => Rails.configuration.twilio_username,
-                                                      :password => Rails.configuration.twilio_password,
-                                                      :from => Rails.configuration.twilio_number)
-twilio_voice_sender = VoiceVendor.find_or_create_by_name!(:name => 'Twilio Voice Sender',
-                                                          :worker => 'TwilioVoiceWorker',
-                                                          :username => Rails.configuration.twilio_username,
-                                                          :password => Rails.configuration.twilio_password,
-                                                          :from => Rails.configuration.twilio_number)
-sms_loopback = SmsVendor.find_or_create_by_name!(:name => 'Loopback SMS Sender',
-                                                 :worker => 'LoopbackSmsWorker',
-                                                 :username => 'sms_loopback_username',
-                                                 :password => 'dont care',
-                                                 :from => '+15551112222')
-voice_loopback = VoiceVendor.find_or_create_by_name!(:name => 'Loopback Voice Sender',
-                                                     :worker => 'LoopbackVoiceWorker',
-                                                     :username => 'voice_loopback_username',
-                                                     :password => 'dont care',
-                                                     :from => '1555111222')
+twilio_sms_sender = SmsVendor.find_or_initialize_by(name: 'Twilio Sender')
+twilio_sms_sender.update_attributes(
+                                                      worker: 'TwilioMessageWorker',
+                                                      username: Rails.configuration.twilio_username,
+                                                      password: Rails.configuration.twilio_password,
+                                                      from: Rails.configuration.twilio_number)
+twilio_voice_sender = VoiceVendor.find_or_initialize_by(name: 'Twilio Voice Sender')
+twilio_voice_sender.update_attributes(
+                                                          worker: 'TwilioVoiceWorker',
+                                                          username: Rails.configuration.twilio_username,
+                                                          password: Rails.configuration.twilio_password,
+                                                          from: Rails.configuration.twilio_number)
+sms_loopback = SmsVendor.find_or_initialize_by(name: 'Loopback SMS Sender')
+sms_loopback.update_attributes(
+                                                 worker: 'LoopbackSmsWorker',
+                                                 username: 'sms_loopback_username',
+                                                 password: 'dont care',
+                                                 from: '+15551112222')
+voice_loopback = VoiceVendor.find_or_initialize_by(name: 'Loopback Voice Sender')
+voice_loopback.update_attributes(
+                                                     worker: 'LoopbackVoiceWorker',
+                                                     username: 'voice_loopback_username',
+                                                     password: 'dont care',
+                                                     from: '1555111222')
 
 
-odm_sender = EmailVendor.find_or_create_by_name!(
-    :name => 'TMS Extended Sender',
-    :worker => Odm::TMS_EXTENDED_WORKER)
+odm_sender = EmailVendor.find_or_initialize_by(
+    name: 'TMS Extended Sender')
+odm_sender.update_attributes(
+    worker: Odm::TMS_EXTENDED_WORKER)
 
-email_loopback = EmailVendor.find_or_create_by_name!(
-    :name => 'Email Loopback Sender',
-    :worker => 'LoopbackEmailWorker')
+email_loopback = EmailVendor.find_or_initialize_by(
+    name: 'Email Loopback Sender')
+email_loopback.update_attributes(
+    worker: 'LoopbackEmailWorker')
 
 if Rails.env.development? || Rails.env.ci?
   #
@@ -52,59 +58,65 @@ if Rails.env.development? || Rails.env.ci?
 
   omg = if (ENV['USE_TWILIO'] == 'true')
           puts "** using Twilio and ODM senders for default account **"
-          Account.find_or_initialize_by_name(voice_vendor: twilio_voice_sender,
-                                             sms_vendor:   twilio_sms_sender,
-                                             email_vendor: odm_sender,
-                                             name:         "OMG")
+          a = Account.find_or_initialize_by(name: "OMG")
+          a.update_attributes(
+                                        voice_vendor: twilio_voice_sender,
+                                        sms_vendor:   twilio_sms_sender,
+                                        email_vendor: odm_sender)
+                                        a
         else
           puts "**  using loopback senders for default account   **"
           puts "** run with USE_TWILIO=true to use Twilio/ODM senders **"
-          Account.find_or_initialize_by_name(:voice_vendor => voice_loopback,
-                                             :sms_vendor   => sms_loopback,
-                                             :email_vendor => email_loopback,
-                                             :name         => "OMG")
+          a = Account.find_or_initialize_by(name: "OMG")
+          a.update_attributes(
+                                             voice_vendor: voice_loopback,
+                                             sms_vendor:   sms_loopback,
+                                             email_vendor: email_loopback,
+                                             )
+                                             a
         end
   unless omg.default_from_address
-    omg.from_addresses.build(:from_email => 'tms_dev@evotest.govdelivery.com',
-                           :errors_to => 'errors@evotest.govdelivery.com',
-                           :reply_to => 'reply@evotest.govdelivery.com',
-                           :is_default => true)
+    omg.from_addresses.build(from_email: 'tms_dev@evotest.govdelivery.com',
+                           errors_to: 'errors@evotest.govdelivery.com',
+                           reply_to: 'reply@evotest.govdelivery.com',
+                           is_default: true)
   end
   omg.save!
 
   # stop requests to this account will spray out to DCM accounts ACME and VANDELAY
   omg.dcm_account_codes = Set.new(['ACME', 'VANDELAY'])
   omg.save!
-  omg.create_command!('Keywords::AccountStop', :params => CommandParameters.new(:dcm_account_codes => ['ACME', 'VANDELAY']), :command_type => :dcm_unsubscribe)
+  omg.create_command!('Keywords::AccountStop', params: CommandParameters.new(dcm_account_codes: ['ACME', 'VANDELAY']), command_type: :dcm_unsubscribe)
 
   # SERVICES FOO => POST to http://localhost/forward
   Keyword.delete_all
-  kw = Keyword.new(:name => 'SERVICES')
+  kw = Keyword.new(name: 'SERVICES')
   kw.account = omg
   kw.vendor = omg.sms_vendor
   kw.save!
-  kw.create_command!(:params => CommandParameters.new(
-    :username => "example@evotest.govdelivery.com",
-    :password => "password",
-    :url => "http://localhost/forward",
-    :http_method => "POST",
-    :sms_body_param_name => "sms_body1"), :command_type => :forward)
+  kw.create_command!(params: CommandParameters.new(
+    username: "example@evotest.govdelivery.com",
+    password: "password",
+    url: "http://localhost/forward",
+    http_method: "POST",
+    sms_body_param_name: "sms_body1"), command_type: :forward)
 
   # SUBSCRIBE ANTHRAX => evolution API request to localhost:3001
-  kw = Keyword.new(:name => 'SUBSCRIBE')
+  kw = Keyword.new(name: 'SUBSCRIBE')
   kw.account = omg
   kw.vendor = omg.sms_vendor
   kw.save!
-  kw.create_command!(:params => CommandParameters.new(:dcm_account_code => "ACME", :dcm_topic_codes => ['ANTRHAX']), :command_type => :dcm_subscribe)
+  kw.create_command!(params: CommandParameters.new(dcm_account_code: "ACME", dcm_topic_codes: ['ANTRHAX']), command_type: :dcm_subscribe)
 
   # Respond to "DONKEY" with "hee-haw!"
-  kw = Keyword.new(:name => "DONKEY", :response_text => "hee-haw!")
+  kw = Keyword.new(name: "DONKEY", response_text: "hee-haw!")
   kw.account = omg
   kw.vendor = omg.sms_vendor
   kw.save!
 
   # Make the product user (admin)
-  user = User.find_or_create_by_email(:email => "product@govdelivery.com", :password => "retek01!")
+  user = User.find_or_initialize_by(email: "product@govdelivery.com")
+  user.update_attributes(password: "retek01!")
   user.account = omg
   user.admin = true
   user.save!
@@ -112,13 +124,13 @@ if Rails.env.development? || Rails.env.ci?
 
 elsif Account.count == 0 && User.count ==0
   puts "#{Rails.env} DB looks empty, creating a GovDelivery account."
-  account= Account.new(:voice_vendor => twilio_voice_sender,
-                           :sms_vendor => twilio_sms_sender,
-                           :email_vendor => odm_sender,
-                           :name => 'GovDelivery')
-  account.build_default_from_address(:from_email => 'info99@service.govdelivery.com')
+  account= Account.new(voice_vendor: twilio_voice_sender,
+                           sms_vendor: twilio_sms_sender,
+                           email_vendor: odm_sender,
+                           name: 'GovDelivery')
+  account.build_default_from_address(from_email: 'info99@service.govdelivery.com')
   account.save!
-  user = account.users.create!(:email => "product@evotest.govdelivery.com", :password => "retek01!")
+  user = account.users.create!(email: "product@evotest.govdelivery.com", password: "retek01!")
   user.admin = true
   user.save!
 else
