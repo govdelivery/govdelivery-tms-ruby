2ef72f2d80f36fe8dc9a980c4551a26fea4a73e1
Benjamin Ortega <ben.ortega@gmail.com>
2014-09-09 14:37:58 -0500

    XACT-358 don’t blow up messages going from new to sending, but also make sure messages are queued before Sidekiq workers actually send them

--- spec/workers/twilio_voice_worker_spec.rb
+++ spec/workers/twilio_voice_worker_spec.rb
@@ -8,17 +8,23 @@ describe TwilioVoiceWorker do
   #need to add recipient stubs and verify recipients are modified correctly
   context 'a send' do
     it 'should create a batch job' do
+      message.class.any_instance.stubs(:queued?).returns(true)
       message.class.any_instance.expects(:sending!)
 
       Twilio::SenderWorker.expects(:perform_async).with(
-        message_class: message.class.name,
-        callback_url: nil,
-        message_url: nil,
-        message_id: message.id,
-        recipient_id: message.recipients.first.id)
+          message_class: message.class.name,
+          callback_url: nil,
+          message_url:  nil,
+          message_id: message.id,
+          recipient_id: message.recipients.first.id)
 
       subject.perform(message_id: message.id)
     end
+  end
+
+  it 'should fail if not in sending state' do
+    Twilio::SenderWorker.expects(:perform_async).never
+    expect { subject.perform(message_id: message.id) }.to raise_error Sidekiq::Retries::Retry
   end
 
 end
