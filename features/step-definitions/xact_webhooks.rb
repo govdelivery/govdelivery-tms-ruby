60e20ab329d604efd00ade8379de67d87a9814ee
Mike Sutehall <mike.sutehall@govdelivery.com>
2014-09-11 13:18:22 -0500

    updated qc tests

--- features/step-definitions/xact_webhooks.rb
+++ features/step-definitions/xact_webhooks.rb
@@ -1,7 +1,31 @@
+def env
+    if ENV['XACT_ENV'] == 'qc'
+        client = TMS::Client.new('auth_token', :api_root => 'https://stage-tms.govdelivery.com')
+    elsif ENV['XACT_ENV'] == 'int'
+        "http://int-tms.govdelivery.com"
+    elsif ENV['XACT_ENV'] == 'stage'
+        "http://stage-tms.govdelivery.com"
+    elsif ENV['XACT_ENV'] == 'prod'
+        "http://tms.govdelivery.com"
+    end
+end
 
+def url
+    if ENV['XACT_ENV'] == 'qc'
+        "http://qc-tms.govdelivery.com"
+    elsif ENV['XACT_ENV'] == 'int'
+        "http://int-tms.govdelivery.com"
+    elsif ENV['XACT_ENV'] == 'stage'
+        "http://stage-tms.govdelivery.com"
+    elsif ENV['XACT_ENV'] == 'prod'
+        "http://tms.govdelivery.com"
+    end
+end
 
 When(/^placeholder$/) do
-  puts 'placeholder'
+  "#{env}"
+  webhook = client.webhooks.build(:url=>"#{url}", :event_type=>'inconclusive')
+  webhook.post
 end
 
 Then(/^something$/) do
