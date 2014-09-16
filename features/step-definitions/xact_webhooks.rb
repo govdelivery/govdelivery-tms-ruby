6e19c2db3c92d3651fa27693ee8ca00651890561
Bill Bushey <bill.bushey@govdelivery.com>
2014-09-16 15:25:29 -0500

    Roughly wrote the Then step to test if the callback endpoints are being hit

--- features/step-definitions/xact_webhooks.rb
+++ features/step-definitions/xact_webhooks.rb
@@ -72,9 +72,21 @@ end
 
 Then(/^the callback registered for each event state should receive a POST referring to the appropriate message$/) do
   @message.recipients.get
+  @message.recipients.collection.each |recipient|
+    status = recipient.attributes[:status]
+    event_callback_uri = @event_callback_uris[status]
+    event_callback = capi.get(event_callback_uri)
+    raise "#{status} callback endpoint should have at least 1 payload" if event_callback["payload_count"] == 0
+    # TODO: Figure out what to do if recipients list does not get build - is that a test failure?
+    passed = false
+    event_callback["payloads"].each |payload_info|
+      payload = capi.get(payload_info["url"])
+      passed = true if payload["payload"]["recipient_url"] == recipient.href
+      # TODO: recipient.href does not have the full URL, should be adding the api_root of tms_client for the above comparison
+    end
+    raise "#{status} callback endpoint does not have a payload referring to the message it should" if not passed
+  end
 end
-
-
 
 
 
