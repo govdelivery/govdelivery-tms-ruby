69cd4f768c76879b77dd106d3784e95bada4835a
Bill Bushey <bill.bushey@govdelivery.com>
2014-09-16 10:05:10 -0500

    Cleaned up some names, trying to create and destroy callback uris around scenarios

--- features/support/webhooks_hooks.rb
+++ features/support/webhooks_hooks.rb
@@ -2,3 +2,7 @@ Before('@webhooks') do
     @capi = Callbacks_API_Client.new(callbacks_api_root)
     puts @capi.callbacks_root
 end
+
+After('@webhooks') do
+    @capi.destroy_all_callback_uris
+end
