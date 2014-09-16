dfb7a6a8e3ea3f54122cf97affeef61f9a1f299c
Mike Sutehall <mike.sutehall@govdelivery.com>
2014-09-16 15:06:46 -0500

    updated webhooks test

--- features/support/webhooks_hooks.rb
+++ features/support/webhooks_hooks.rb
@@ -1,8 +1,9 @@
-Before('@webhooks') do
+Before do |scenario|
+	puts "Starting scenario: "
     @capi = Callbacks_API_Client.new(callbacks_api_root)
     puts @capi.callbacks_root
 end
 
-After('@webhooks') do
-    @capi.destroy_all_callback_uris
-end
+#After('@webhooks') do
+    #@capi.destroy_all_callback_uris
+#end
