a855e66c4bf9e1771b37f2a3a9bcfeb2ec2931f5
Benjamin Ortega <ben.ortega@gmail.com>
2014-09-26 11:38:10 -0500

    XACT-373 remove batch progress lock since the jobs should be idempotent anyway

--- app/workers/messages/check_messages_for_completion.rb
+++ app/workers/messages/check_messages_for_completion.rb
@@ -3,21 +3,12 @@ require 'base'
 module Messages
   class CheckMessagesForCompletion
     include Workers::Base
-    sidekiq_options queue: :low, retry: false
-    JOB_KEY = "xact:#{self.name}_bid"
-
-    class OnComplete
-      def on_complete(status, options)
-        Rails.cache.delete(JOB_KEY)
-      end
-    end
+    sidekiq_options queue: :low, retry: false, unique: true
 
     def perform(*args)
-      return if batch_in_progress?
       batch             = Sidekiq::Batch.new
       batch.description = self.class.name
       batch.expires_in 8.hours
-      batch.on(:success, OnComplete, 'job_key' => JOB_KEY)
       batch.jobs do
         [SmsMessage, VoiceMessage, EmailMessage].each do |message_class|
           message_scope(message_class).find_each do |message|
@@ -25,10 +16,6 @@ module Messages
           end
         end
       end
-    end
-
-    def batch_in_progress?
-      Rails.cache.read(JOB_KEY)
     end
 
     def message_scope(message_class)
