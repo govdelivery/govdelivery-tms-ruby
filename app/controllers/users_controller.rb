fdc49733c44410297a81198e1249d733590215eb
Bill Bushey <bill.bushey@govdelivery.com>
2014-10-01 15:14:35 -0500

    XACT-366 Added array of tokens to listing of users in users endpoint on accounts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
+require 'ostruct'
+
 class UsersController < ApplicationController
   before_filter ->(c) { render(json: {error: "forbidden"},
                                status: :forbidden) unless current_user.admin? }
 
   def index
-    @account = Account.find(params[:account_id])
+    account_obj = Account.find(params[:account_id])
+    @account = account_obj.attributes
+    @account['users'] = []
+    account_obj.users.each do |user_obj|
+      user = user_obj.attributes
+      user.delete('encrypted_password')
+      user['tokens'] = user_obj.authentication_tokens.map(&:token)
+      @account['users'] << user
+    end
+    @account = OpenStruct.new(@account)
   end
 end
