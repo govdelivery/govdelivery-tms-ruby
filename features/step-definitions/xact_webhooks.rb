7108ae66d1bdce5030ce54bd2314d931e6446052
Bill Bushey <bill.bushey@govdelivery.com>
2014-10-06 15:25:08 -0500

    Implemented an exponential backoff for the check on recipient list being finished No longer creating 3 webhooks per event_type, since that doesn't actually work

--- features/step-definitions/xact_webhooks.rb
+++ features/step-definitions/xact_webhooks.rb
@@ -7,32 +7,25 @@ $subject.store(1, Time.new) #storing the hash value so we can retrieve it later 
 
 
 Given(/^all message types$/) do
-  @message_event_callback_uris = Hash[message_types.map {|message_type| [message_type,nil]}]
 end
 
 Given(/^all event types$/) do
-  @message_event_callback_uris.each_key do |message_type|
-    @message_event_callback_uris[message_type] = Hash[event_types.map {|event_type| [event_type,nil]}]
+  @event_callback_uris = Hash[event_types.map {|event_type| [event_type,nil]}]
+end
+
+Then(/^a callback url exists for each event type$/) do
+  @event_callback_uris.each_key do |event_type|
+    @event_callback_uris[event_type] = @capi.create_callback_uri(event_type)
   end
 end
 
-Then(/^a callback url exists for each message type and event type$/) do
-    @message_event_callback_uris.each do |message_type, event_callback_uris|
-      event_callback_uris.each_key do |event_type|
-        event_callback_uris[event_type] = @capi.create_callback_uri("#{message_type}-#{event_type}")
-      end
-    end
-end
-
-And(/^a callback url is registered for each message type and event type$/) do
+And(/^a callback url is registered for each event type$/) do
   client = tms_client
-  @message_event_callback_uris.each do |message_type, event_callback_uris|
-    event_callback_uris.each do |key,value|
-      webhook = client.webhooks.build(:url=>@capi.callbacks_domain + value, :event_type=>key)
-      webhook.post
-      puts "Webhook registered for #{message_type}-#{key}: #{value}"
-      @webhooks << webhook
-    end
+  @event_callback_uris.each do |key,value|
+    webhook = client.webhooks.build(:url=>@capi.callbacks_domain + value, :event_type=>key)
+    webhook.post
+    puts "Webhook registered for #{key}: #{value}"
+    @webhooks << webhook
   end
 end
 
@@ -64,18 +57,43 @@ When(/^I send a message of each type to the magic address of each event state$/)
 end
 
 Then(/^the callback registered for each event state should receive a POST referring to the appropriate message$/) do
-  sleep(30)
+
+
+  message_map = {:email => @email_message, :sms => @sms_message, :voice => @voice_message}
+  slept_time = 0
+  slept_messages = Hash[message_types.map {|message_type| [message_type,false]}]
+
+  # 2 ^ 8 = ~ 4.2 minutes
+  for x in 0..8
+    sleep_time = 2 ** x
+    puts "Waiting #{sleep_time} seconds for recipients lists to build"
+    sleep(sleep_time)
+    slept_time += sleep_time
+    slept_messages.each do |message_type, got|
+      if not got
+        begin
+          slept_messages[message_type] = message_map[message_type].recipients.get
+        rescue TMS::Request::InProgress
+          puts "Recipient list for #{message_type} is not ready"
+        end
+      end
+    end
+    break if slept_messages.all?{|event_type, got| got}
+    raise "Recipients list has not built within a reasonable time. Have waited #{slept_time} seconds" if x == 8
+  end
+  puts "Total time waited for recipients list to build: #{slept_time}"
   # TODO: Sleep shouldn't fix our problems
   # TODO: Figure out what to do if recipients list does not get build - is that a test failure?
 
-  {:email => @email_message, :sms => @sms_message, :voice => @voice_message}.each do |message_type, message|
+  message_map.each do |message_type, message|
     message.recipients.get
     message.recipients.collection.each do |recipient|
       status = recipient.attributes[:status]
       puts "#{message_type} | Current Status: #{status} | URL: #{recipient.href}"
-      event_callback_uri = @message_event_callback_uris[message_type][status]
+      #event_callback_uri = @message_event_callback_uris[message_type][status]
+      event_callback_uri = @event_callback_uris[status]
       event_callback = @capi.get(event_callback_uri)
-      puts "Checking webhook registered for #{message_type}-#{status}: #{event_callback_uri}"
+      puts "Checking webhook registered for #{status}: #{event_callback_uri}"
       raise "Callback endpoint for #{message_type} #{recipient.href} should have at least 1 payload\n#{message_type}-#{status} callback endpoint: #{event_callback}" if event_callback["payload_count"] == 0
       raise "Callback endpoint for #{message_type} #{recipient.href} should have non-nil payloads\n#{message_type}-#{status} callback endpoint: #{event_callback}" if event_callback["payloads"].nil?
       passed = false
@@ -83,14 +101,20 @@ Then(/^the callback registered for each event state should receive a POST referr
       condition = xact_url + recipient.href
       event_callback["payloads"].each do |payload_info|
         payloads << @capi.get(payload_info["url"])
-        foo = payloads[-1]["payload"]["recipient_url"]
-        passed = true if payloads[-1]["payload"]["recipient_url"] == condition
+        #foo = payloads[-1]["payload"]["recipient_url"]
+        #passed = true if payloads[-1]["payload"]["recipient_url"] == condition
+        passed = payloads.any?{|payload| payload["payload"][recipient_url] == condition}
       end
-    raise "#{status} callback endpoint does not have a payload referring to #{condition}\npayloads: #{JSON.pretty_generate(payloads)}" if not passed
+      raise "#{status} callback endpoint does not have a payload referring to #{condition}\npayloads: #{JSON.pretty_generate(payloads)}" if not passed
     end
   end
 end
 
+
+
+
+
+### Don't believe these are used
 Then(/^something$/) do
   puts 'Arby\'s nation.'
 end
