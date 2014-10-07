315187d0d470160c57c681de7af4b6a9c549f28c
Benjamin Ortega <ben.ortega@gmail.com>
2014-10-07 14:39:25 -0500

    fix ActiveModel::MissingAttributeError in QC

--- app/workers/twilio_voice_worker.rb
+++ app/workers/twilio_voice_worker.rb
@@ -13,7 +13,7 @@ class TwilioVoiceWorker
     callback_url = options[:callback_url]
     message_url = options[:message_url]
 
-    if message = VoiceMessage.select('id, account_id, play_url, status').find(message_id)
+    if message = VoiceMessage.select('id, account_id, play_url, status, user_id').find(message_id)
       raise Sidekiq::Retries::Retry.new(RuntimeError.new("#{message.class.name} #{message.id} is not ready for delivery!")) unless message.queued?
       logger.info("Send initiated for message_id=#{message.id} and callback_url=#{callback_url}")
       Service::TwilioMessageService.deliver!(message, callback_url, message_url)
