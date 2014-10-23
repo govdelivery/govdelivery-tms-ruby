7c508745fe4f589d1135be7bd26f8f571bd7c8eb
Benjamin Ortega <ben.ortega@gmail.com>
2014-10-23 13:19:18 -0500

    XACT-398 use pessimistic lock for message completion check

--- app/workers/messages/check_message_for_completion.rb
+++ app/workers/messages/check_message_for_completion.rb
@@ -9,11 +9,13 @@ module Messages
 
     def perform(args)
       klass = args['message_class'].constantize
-      if (message = klass.without_message.find(args['message_id']))
+      if (message = klass.lock('FOR UPDATE SKIP LOCKED').without_message.find(args['message_id']))
         return if message.completed?
         logger.debug("Checking completion status for message #{message.class.name} #{message.id}")
         message.complete!
       end
+      rescue ActiveRecord::RecordNotFound => e
+        false
     end
   end
 end
--- spec/workers/messages/check_message_for_completion_spec.rb
+++ spec/workers/messages/check_message_for_completion_spec.rb
@@ -5,17 +5,22 @@ describe Messages::CheckMessageForCompletion do
 
   it 'should work on a message' do
     message=mock('message', :completed? => false, :complete! => false, id: 7)
-
-    SmsMessage.expects(:without_message).returns(mock('scope', find: message))
+    lock   = mock('lock', without_message: mock('scope', find: message))
+    SmsMessage.expects(:lock).returns(lock)
 
     subject.perform('message_class' => 'SmsMessage', 'message_id' => 7)
   end
 
   it 'should silently exit if message is already complete' do
     message=mock('message', :completed? => true)
-
-    SmsMessage.expects(:without_message).returns(mock('scope', find: message))
+    lock   = mock('lock', without_message: mock('scope', find: message))
+    SmsMessage.expects(:lock).returns(lock)
 
     subject.perform('message_class' => 'SmsMessage', 'message_id' => 7)
+  end
+
+  it 'should silently exit if record is locked' do
+    SmsMessage.expects(:lock).raises(ActiveRecord::RecordNotFound)
+    expect(subject.perform('message_class' => 'SmsMessage', 'message_id' => 7)).to be false
   end
 end
