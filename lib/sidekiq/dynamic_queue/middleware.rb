b9c346f7bf882420dc71a96291b8b1935927f5ed
Benjamin Ortega <ben.ortega@gmail.com>
2014-10-31 11:03:09 -0500

    XACT-394 fix dynamic queue checking fail

--- lib/sidekiq/dynamic_queue/middleware.rb
+++ lib/sidekiq/dynamic_queue/middleware.rb
@@ -14,9 +14,8 @@ module Sidekiq
 
         def queue_for(worker, msg, queue)
           return queue unless queue_proc = worker.get_sidekiq_options['dynamic_queue_key']
-          queue_suffix = queue_proc.call(*msg['args'])
-          queue_suffix = nil unless Sidekiq::RateLimitedQueue.includes_queue?(queue_suffix)
-          [msg['queue'], queue_suffix].compact.join('_')
+          possible_queue = [msg['queue'], queue_proc.call(*msg['args'])].compact.join('_')
+          Sidekiq::RateLimitedQueue.includes_queue?(possible_queue) ? possible_queue : queue
         end
 
       end
