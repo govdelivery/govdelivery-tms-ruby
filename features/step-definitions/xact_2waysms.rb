04cb710b1b9f6b28eed3721f3592f97b6504d7fa
Mike Sutehall <mike.sutehall@govdelivery.com>
2014-11-10 12:51:21 -0600

    XACT-410: Successfully testing 2-way SMS Real Time quering with a plain text command response (BART) added stuff for 2wayssms subscribe and stop tests
    Conflicts:
    	features/step-definitions/xact_2waysms.rb
    	features/support/env.rb
    	features/xact_2way.feature
    
    Setting the Phone SID of the Twilio Xact Test number
    
    adding xact_2waysms test to suite
    
    Conflicts:
    	features/step-definitions/xact_2waysms.rb
    	features/support/env.rb
    
    Stubbed out BART End to End Test
    
    First attempt to write actual test of BART
    
    BART 2-way Real Time query works on localhost
    Rough configuration to run BART 2-way real time query test on other
    environments
    
    Fixing bart configuration
    Test successfully runs on dev, qc, int, and stage

--- features/step-definitions/xact_2waysms.rb
+++ features/step-definitions/xact_2waysms.rb
@@ -13,6 +13,7 @@ require 'multi_xml'
 #===SUBSCRIBE========================================>
 
 
+
 Given(/^I create a subscription keyword and command$/) do
   client = tms_client(:live)
   @keyword = client.keywords.build(:name => keyword_params, :response_text => keyword_params)
@@ -33,7 +34,6 @@ And(/^I send an SMS to create a subscription on TMS$/) do
     faraday.response    :logger
     faraday.adapter     Faraday.default_adapter
   end
-
   #create tms/xact twilio request
   payload = {}
   payload['To'] = xact_account(:live)[:sms_phone]
@@ -44,17 +44,16 @@ And(/^I send an SMS to create a subscription on TMS$/) do
     req.url "/twilio_requests.xml"
     req.body = payload
   end
-
   #encode FROM number as base64 so we're able to retrieve the subscriber record in DCM subscribers API
   @base64 = Base64.encode64(sample_subscriber_number)
-  sleep(10)
+  sleep(3)
 
   #delete tms/xact keyword and command entirely
   @keyword.delete
 end
 
 Then(/^a subscription should be created$/) do
-  user #dcm credentials
+  user
   @request.url = dcm_base64_url + @base64
   @data = HTTPI.get(@request)
   @response = MultiXml.parse(@data.raw_body)
@@ -75,6 +74,9 @@ Then(/^a subscription should be created$/) do
 end
 
 
+#===STOP========================================>
+
+
 
 #===STOP========================================>
 
@@ -93,10 +95,7 @@ And(/^a my subscription should be removed$/) do
 end
 
 
-
 #===STATIC========================================>
-
-
 
 Given (/^A keyword with static content is configured for an TMS account$/) do
   client = tms_client(:loopback)
@@ -126,4 +125,80 @@ Then (/^I should receive static content$/) do
   received_content = twiml['Response']['Sms']
   expected_content = @keyword.response_text
   raise "Received incorrect content: '#{received_content}', expected: '#{expected_content}', keyword url: #{xact_url}#{@keyword.href}" if received_content != expected_content
+end
+
+
+#===BART========================================>
+
+Given (/^I have an XACT account for BART$/) do
+  @client = tms_client(:bart)
+end
+
+Given (/^I register the keyword BART$/) do
+  # Register a unique keyword each time, so that test failures can save the keyword for review without concern for future test keyword collisions
+  @bart_keyword = "BART#{random_string}"
+  @keyword = @client.keywords.build(:name => @bart_keyword)
+  raise "Could not create #{@bart_keyword} keyword: #{@keyword.errors}" unless @keyword.post
+end
+
+Given (/^I register the BART forward command$/) do
+  @command = @keyword.commands.build(
+    :name => "BART Forwarding",
+    :params => {
+      :url => 'http://ws.sfbart.org/sms/request.aspx',
+      :http_method => 'get',
+      :from_param_name => 'user',
+      :sms_body_param_name => 'req',
+      :strip_keyword => true
+    },
+    :command_type => :forward
+  )
+  raise "Could not create Forwarding command: #{@command.errors}" unless @command.post
+end
+
+When (/^I text 'BART 12th' to the BART account$/) do
+  conn = Faraday.new(:url => "#{xact_url}") do |faraday|
+    faraday.request     :url_encoded
+    faraday.response    :logger
+    faraday.adapter     Faraday.default_adapter
+  end
+  payload = {}
+  payload['To'] = xact_account(:bart)[:sms_phone]
+  payload['From'] = '+15005550006'
+  payload['AccountSid'] = xact_account(:bart)[:sms_vendor_username]
+  payload['Body'] = "#{@bart_keyword} 12th"
+  @resp = conn.post do |req|
+    req.url "/twilio_requests.xml"
+    req.body = payload
+  end
+end
+
+Then (/^I should receive BART content as a response$/) do
+  # TODO Can we find the actual message that XACT sent back?
+  passed = false
+  expected_status = 200
+
+  check = Proc.new do
+    # The API does not provide the command_actions relation on a command if there are no command actions
+    # Thus, we need to be ready to catch a NoMethodError in case a command action has not been created
+    # by the time the test wants to check for one.
+    begin
+      @command.get
+      @command.command_actions.get
+      @actions = @command.command_actions.collection
+    rescue NoMethodError => e
+      next
+    end
+    passed = @actions.any? do |action|
+      action.status == expected_status && !action.response_body.blank?
+    end
+  end
+  check_condition = Proc.new{passed}
+  begin
+    backoff_check(check, check_condition, "for BART to send an acceptable response")
+  rescue => e
+    msg = "Expected to receive HTTP Status #{expected_status} and expected to receive non-blank response_text"
+    msg += "Command URL: #{@command.href}"
+    raise $!, "#{$!}\n#{msg}"
+  end
 end
