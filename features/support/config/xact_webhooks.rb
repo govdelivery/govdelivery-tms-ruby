c47941491a65ba7848c786d59adc4519e45f647c
Bill Bushey <bill.bushey@govdelivery.com>
2014-11-25 14:02:38 -0600

    DRYing Shared Test Loopback Vendor config info in end-to-end tests Reconfiguring Webhooks test to use it's own account

--- features/support/config/xact_webhooks.rb
+++ features/support/config/xact_webhooks.rb
@@ -3,38 +3,29 @@
 configatron.accounts.webhooks.xact.url = configatron.xact.url
 webhooks = configatron.accounts.webhooks
 
-# Many of the webhook test account attributes are set by a rake task that is run on all environments, so most values
-# are shared across environments
+# Set common values across environments, most based on config values for shared vendors
 webhooks.xact.user.password = 'retek01!'
-webhooks.xact.user.admin = true
-webhooks.sms.phone.number = '+15559999999'
-webhooks.sms.phone.sid = nil
-webhooks.sms.vendor.username = 'loopbacks_account_sms_username'
-webhooks.sms.vendor.password = 'dont care'
-webhooks.sms.vendor.shared = false
-webhooks.sms.vendor.twilio_test = false
-webhooks.sms.prefix = nil
-webhooks.voice.phone.number = '+15559999999'
-webhooks.voice.phone.sid = nil
-webhooks.voice.vendor.username = 'loopbacks_account_voice_username'
-webhooks.voice.vendor.password = 'dont care'
-webhooks.voice.twilio_test = false
+webhooks.xact.user.admin = false
+
+webhooks.sms = configatron.sms_vendor.loopback.clone()
+webhooks.voice = configatron.voice_vendor.loopback.clone()
+webhooks.sms.prefix = 'webhooks'
 
 case environment
   when :development
     webhooks.xact.account.id = ENV['XACT_WEBHOOK_ACCOUNT_ID']
     webhooks.xact.user.token = ENV['XACT_WEBHOOK_USER_TOKEN']
-    webhooks.xact.user.email_address = 'development-loopback@govdelivery.com'
+    webhooks.xact.user.email_address = 'development-webhooks-test@govdelivery.com'
   when :qc
     webhooks.xact.account.id = '10460'
     webhooks.xact.user.token = 'sXNsShoQRX1X5qa5ZuegCzL7hUpebSdL'
-    webhooks.xact.user.email_address = 'qc-loopback@govdelivery.com'
+    webhooks.xact.user.email_address = 'qc-webhooks-test@govdelivery.com'
   when :integration
     webhooks.xact.account.id = '10200'
     webhooks.xact.user.token = '7SxUtWmkq5Lsjnw2s5rxJULqrHs37AbE'
-    webhooks.xact.user.email_address = 'int-loopback@govdelivery.com'
+    webhooks.xact.user.email_address = 'int-webhooks-test@govdelivery.com'
   when :stage
     webhooks.xact.account.id = '10862'
     webhooks.xact.user.token = 'CtyXxoinsNHujmfFd2qhKRJvDBMNqmPm'
-    webhooks.xact.user.email_address = 'stage-loopback@govdelivery.com'
+    webhooks.xact.user.email_address = 'stage-webhooks-test@govdelivery.com'
 end
