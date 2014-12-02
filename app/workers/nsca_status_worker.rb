b206014c82f85a305ed660de21076f28b5255b54
Benjamin Ortega <ben.ortega@gmail.com>
2014-12-02 12:24:44 -0600

    XACT-438 use sidekiq logger

--- app/workers/nsca_status_worker.rb
+++ app/workers/nsca_status_worker.rb
@@ -15,7 +15,7 @@ class NscaStatusWorker
         return_code: count==0 ? SendNsca::STATUS_OK : SendNsca::STATUS_WARNING,
         status:      "Status #{count==0 ? 'OK' : 'WARNING'}: #{count} records"
       }
-      Rails.logger.debug("SendNsca::NscaConnection -- #{args.inspect}")
+      logger.debug("SendNsca::NscaConnection -- #{args.inspect}")
       SendNsca::NscaConnection.new(args).send_nsca
     end
   end
