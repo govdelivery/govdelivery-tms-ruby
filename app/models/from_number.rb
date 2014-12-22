bdf726f0e20031a14d83375c1c6350c2898fcd18
smedrick <rwatkins@gov-i.com>
2014-12-22 16:49:20 -0500

    XACT-468 phone number table

--- app/models/from_number.rb
+++ app/models/from_number.rb
@@ -4,14 +4,14 @@ class FromNumber < ActiveRecord::Base
   alias_attribute :from_number, :phone_number
 
   before_validation :normalize_phone_number
-
-  validates :phone_number, presence: true, uniqueness: true
+  validates :phone_number, presence: true, uniqueness: {scope: :account_id}
 
   before_save :ensure_unique_defaultness
 
   ##
   # There should only be one default from number at a given time.
   #
+
   def ensure_unique_defaultness(*args)
     if current_default = account.from_numbers.where(is_default: true).first
       if (self.is_default? && current_default != self)
@@ -21,7 +21,6 @@ class FromNumber < ActiveRecord::Base
       self.is_default = true
     end
   end
-
   def normalize_phone_number
     self.phone_number = PhoneNumber.new(phone_number).e164_or_short
   end
