8d1e04497f9705e11df8f98959ee59f4fbcade82
Benjamin Ortega <ben.ortega@gmail.com>
2015-01-13 13:39:59 -0600

    XACT-286 high finding: “Session secret should not be included in version control”

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Xact::Application.config.secret_token = 'ac020c53879652870cba66c3b615145204f0133d94389bcb08869f7c7cb94e4cf18f34cf0e32b73208c6c54326ce3bbd84b54a20377429ff6da582f8a81e2ca1'
