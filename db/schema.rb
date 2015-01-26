29d48e44d009804e4cf26bc7438793105bfd9acf
Benjamin Ortega <ben.ortega@gmail.com>
2015-01-26 16:54:02 -0600

    XACT-444 update schema.rb after migration

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 20150123165654) do
 
   create_table "accounts", force: true do |t|
     t.string   "name",                                                        null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                                  null: false
+    t.datetime "updated_at",                                                  null: false
     t.integer  "voice_vendor_id",                    precision: 38, scale: 0
     t.integer  "email_vendor_id",                    precision: 38, scale: 0
     t.integer  "sms_vendor_id",                      precision: 38, scale: 0
@@ -30,8 +30,8 @@ ActiveRecord::Schema.define(version: 20150123165654) do
   create_table "authentication_tokens", force: true do |t|
     t.integer  "user_id",    precision: 38, scale: 0, null: false
     t.string   "token",                               null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
   add_index "authentication_tokens", ["token"], name: "i_authentication_tokens_token", unique: true, tablespace: "tsms_indx01"
@@ -39,8 +39,8 @@ ActiveRecord::Schema.define(version: 20150123165654) do
   create_table "call_scripts", force: true do |t|
     t.integer  "voice_message_id",              precision: 38, scale: 0
     t.string   "say_text",         limit: 1000
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                             null: false
+    t.datetime "updated_at",                                             null: false
   end
 
   create_table "command_actions", force: true do |t|
@@ -55,8 +55,8 @@ ActiveRecord::Schema.define(version: 20150123165654) do
   create_table "commands", force: true do |t|
     t.string   "name"
     t.string   "params",       limit: 4000
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                         null: false
+    t.datetime "updated_at",                                         null: false
     t.string   "command_type",                                       null: false
     t.integer  "keyword_id",                precision: 38, scale: 0
   end
@@ -64,26 +64,26 @@ ActiveRecord::Schema.define(version: 20150123165654) do
   add_index "commands", ["keyword_id"], name: "index_commands_on_keyword_id", tablespace: "tsms_indx01"
 
   create_table "email_messages", force: true do |t|
+    t.string   "status",                                                      default: "new", null: false
+    t.datetime "created_at",                                                                  null: false
+    t.datetime "updated_at",                                                                  null: false
+    t.boolean  "open_tracking_enabled",              precision: 1,  scale: 0, default: true
+    t.boolean  "click_tracking_enabled",             precision: 1,  scale: 0, default: true
     t.integer  "user_id",                            precision: 38, scale: 0
     t.integer  "account_id",                         precision: 38, scale: 0,                 null: false
-    t.text     "body"
-    t.string   "status",                                                      default: "new", null: false
     t.string   "from_name"
     t.string   "subject",                limit: 400
     t.datetime "completed_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
     t.string   "ack"
     t.datetime "sent_at"
-    t.boolean  "open_tracking_enabled",              precision: 1,  scale: 0, default: true
-    t.boolean  "click_tracking_enabled",             precision: 1,  scale: 0, default: true
+    t.text     "body"
     t.text     "macros"
     t.string   "from_email"
     t.string   "reply_to"
     t.string   "errors_to"
   end
 
-  add_index "email_messages", ["user_id", "created_at", "status", "subject", "id"], name: "em_idx4", tablespace: "tsms_indx01"
+  add_index "email_messages", ["user_id", "created_at", "status", "subject", "id"], name: "em_idx3", tablespace: "tsms_indx01"
 
   create_table "email_recipient_clicks", force: true do |t|
     t.integer  "email_message_id",                precision: 38, scale: 0, null: false
@@ -118,8 +118,8 @@ ActiveRecord::Schema.define(version: 20150123165654) do
     t.string   "error_message", limit: 512
     t.datetime "sent_at"
     t.datetime "completed_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                                         null: false
+    t.datetime "updated_at",                                                         null: false
     t.text     "macros"
   end
 
@@ -128,11 +128,31 @@ ActiveRecord::Schema.define(version: 20150123165654) do
   create_table "email_vendors", force: true do |t|
     t.string   "name",                null: false
     t.string   "worker",              null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.string   "deliveries_sequence"
     t.string   "clicks_sequence"
     t.string   "opens_sequence"
+  end
+
+  create_table "fix_email_messages", force: true do |t|
+    t.string   "status",                                                      default: "new", null: false
+    t.datetime "created_at",                                                                  null: false
+    t.datetime "updated_at",                                                                  null: false
+    t.integer  "account_id",                         precision: 38, scale: 0,                 null: false
+    t.boolean  "open_tracking_enabled",              precision: 1,  scale: 0, default: true
+    t.boolean  "click_tracking_enabled",             precision: 1,  scale: 0, default: true
+    t.integer  "user_id",                            precision: 38, scale: 0
+    t.string   "from_name"
+    t.string   "from_email"
+    t.string   "reply_to"
+    t.string   "errors_to"
+    t.string   "ack"
+    t.string   "subject",                limit: 400
+    t.datetime "completed_at"
+    t.datetime "sent_at"
+    t.text     "body"
+    t.text     "macros"
   end
 
   create_table "from_addresses", force: true do |t|
@@ -155,8 +175,8 @@ ActiveRecord::Schema.define(version: 20150123165654) do
     t.integer  "vendor_id",                    precision: 38, scale: 0
     t.string   "caller_phone"
     t.string   "body",             limit: 300
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                            null: false
+    t.datetime "updated_at",                                            null: false
     t.string   "vendor_phone",     limit: 100
     t.integer  "keyword_id",                   precision: 38, scale: 0
     t.string   "keyword_response"
@@ -181,15 +201,15 @@ ActiveRecord::Schema.define(version: 20150123165654) do
     t.text     "public_password_encrypted",                           null: false
     t.text     "private_password_encrypted",                          null: false
     t.binary   "jks",                                                 null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                          null: false
+    t.datetime "updated_at",                                          null: false
   end
 
   create_table "keywords", force: true do |t|
     t.integer  "account_id",                precision: 38, scale: 0, null: false
     t.string   "name",          limit: 160,                          null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                         null: false
+    t.datetime "updated_at",                                         null: false
     t.string   "response_text", limit: 160
   end
 
@@ -208,8 +228,8 @@ ActiveRecord::Schema.define(version: 20150123165654) do
     t.string   "prefix",                                 null: false
     t.integer  "account_id",    precision: 38, scale: 0, null: false
     t.integer  "sms_vendor_id", precision: 38, scale: 0, null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
   end
 
   create_table "sms_recipients", force: true do |t|
@@ -233,8 +253,8 @@ ActiveRecord::Schema.define(version: 20150123165654) do
     t.string   "password",                                                                                                                                                                         null: false
     t.string   "from_phone",                                                                                                                                                                       null: false
     t.string   "worker",                                                                                                                                                                           null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                                                                                                                                                       null: false
+    t.datetime "updated_at",                                                                                                                                                                       null: false
     t.string   "help_text",                                     default: "This service is provided by GovDelivery. If you are a customer in need of assistance, please contact customer support.", null: false
     t.string   "stop_text",                                     default: "You will no longer receive SMS messages.",                                                                               null: false
     t.boolean  "shared",                precision: 1, scale: 0, default: false,                                                                                                                    null: false
@@ -247,8 +267,8 @@ ActiveRecord::Schema.define(version: 20150123165654) do
   create_table "stop_requests", force: true do |t|
     t.integer  "vendor_id",  precision: 38, scale: 0, null: false
     t.string   "phone",                               null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.integer  "account_id", precision: 38, scale: 0
   end
 
@@ -259,8 +279,8 @@ ActiveRecord::Schema.define(version: 20150123165654) do
     t.string   "email",                                                       null: false
     t.string   "encrypted_password",                                          null: false
     t.boolean  "admin",              precision: 1,  scale: 0, default: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                                  null: false
+    t.datetime "updated_at",                                                  null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, tablespace: "tsms_indx01"
@@ -307,8 +327,8 @@ ActiveRecord::Schema.define(version: 20150123165654) do
     t.string   "username",   null: false
     t.string   "password",   null: false
     t.string   "worker",     null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "webhooks", force: true do |t|
