d4d20864b09cad55d73d8588b0ce0e2985b90d56
Benjamin Ortega <ben.ortega@gmail.com>
2015-01-28 23:41:05 -0600

    TwilioStatusCallbacksController: Log when we enqueue call retries. don’t swallow other exceptions

--- app/controllers/twilio_status_callbacks_controller.rb
+++ app/controllers/twilio_status_callbacks_controller.rb
@@ -7,17 +7,17 @@ class TwilioStatusCallbacksController < ApplicationController
   def create
     begin
       @recipient.send(transition, @sid, nil, secondary_status)
-    rescue Recipient::ShouldRetry  #call came back as busy, no answer, or fail...retry
+    rescue Recipient::ShouldRetry #call came back as busy, no answer, or fail...retry
       if @recipient.sending?
-        args = {message_id: @recipient.message.id,
+        logger.info("retrying #{@recipient.class.name} #{@recipient.id} attempt #{@recipient.retries} (#{transition} - #{secondary_status})")
+        args = {message_id:   @recipient.message.id,
                 recipient_id: @recipient.id,
-                message_url: twiml_url,
-                callback_url: twilio_status_callbacks_url(:format => :xml)}
+                message_url:  twiml_url,
+                callback_url: twilio_status_callbacks_url(format: :xml)}
         @recipient.message.worker.perform_in(@recipient.message.retry_delay.seconds, args)
       end
-    ensure
-      render :text => '', :status => 201
     end
+    render text: '', status: 201
   end
 
   protected
@@ -30,14 +30,14 @@ class TwilioStatusCallbacksController < ApplicationController
   end
 
   def find_recipient
-    @recipient=if params.has_key?('SmsStatus')
-                 @sid = params['SmsSid']
-                 SmsRecipient.find_by_ack!(@sid)
-               elsif params.has_key?('CallStatus')
-                 @sid = params['CallSid']
-                 VoiceRecipient.find_by_ack!(@sid)
-               else
-                 nil
-               end
+    @recipient = if params.has_key?('SmsStatus')
+                   @sid = params['SmsSid']
+                   SmsRecipient.find_by_ack!(@sid)
+                 elsif params.has_key?('CallStatus')
+                   @sid = params['CallSid']
+                   VoiceRecipient.find_by_ack!(@sid)
+                 else
+                   nil
+                 end
   end
 end
