da97def1fdae4fd64faabb6e10312801d1cd5392
Benjamin Ortega <ben.ortega@gmail.com>
2015-02-20 14:47:18 -0600

    XACT-513 retry if Analytics::PublisherWorker times out waiting for a connection. We’ll need this for blacklist publishing anyway.

--- app/workers/analytics/publisher_worker.rb
+++ app/workers/analytics/publisher_worker.rb
@@ -2,26 +2,28 @@ require 'base'
 module Analytics
   class PublisherWorker
     include Workers::Base
-    sidekiq_options retry: false
+    sidekiq_options retry: 0
+    sidekiq_retry_in { 10 }
 
     CONNECTION_POOL = ConnectionPool.new(size: 5, timeout: 5) do
       YaketyYak::Publisher.new('xact')
     end
 
     def perform(opts)
-      if Rails.configuration.analytics[:enabled]
-        opts.symbolize_keys!
-        validate_opts(opts)    
-        channel = opts[:channel]
-        message = opts[:message].merge(:src => 'xact')
-        CONNECTION_POOL.with do |connection|
-          Rails.logger.info("#{self.class}: Publishing #{channel} #{message}")
-          connection.publish(channel, message)
-        end
+      return unless Rails.configuration.analytics[:enabled]
+      validate_opts(opts.symbolize_keys!)
+      channel = opts[:channel]
+      message = opts[:message].merge(src: 'xact')
+      CONNECTION_POOL.with do |connection|
+        logger.info("#{self.class}: Publishing #{channel} #{message}")
+        connection.publish(channel, message)
       end
+    rescue Timeout::Error => e
+      raise Sidekiq::Retries::Retry.new(e)
     end
 
-  private
+    private
+
     def validate_opts(opts)
       unless opts[:channel] && opts[:message]
         raise ArgumentError.new("Expected #{opts} to have :channel and :message")
