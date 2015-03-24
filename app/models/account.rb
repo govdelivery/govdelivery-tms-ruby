b5b6f619c02fc28fa5104c575f09ff5af02cd9e2
Bill Bushey <bill.bushey@govdelivery.com>
2015-03-24 13:17:34 -0500

    XACT-518: Turned link_tracking_parameters_hash into 1 line

--- app/models/account.rb
+++ app/models/account.rb
@@ -126,11 +126,7 @@ class Account < ActiveRecord::Base
   # Make link tracking parameters act just like they do in Evo
   #
   def link_tracking_parameters_hash
-    if link_tracking_parameters.nil?
-      {}
-    else
-      GovDelivery::Links::Transformer.querystring_to_hash('?' + link_tracking_parameters)
-    end
+    GovDelivery::Links::Transformer.querystring_to_hash('?' + (link_tracking_parameters || ''))
   end
 
   def destroy
