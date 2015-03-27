c04c6a609c7dd227b399e2ad608af2ccd5ecc5d6
Benjamin Ortega <ben.ortega@gmail.com>
2015-03-27 15:54:15 -0500

    for brosie

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,11 @@
 #   rake db:seed
 ##
 
-raise "TWILIO_SID not set" unless twilio_username = ENV['TWILIO_SID'].try(:dup)
-raise "TWILIO_TOKEN not set" unless twilio_password = ENV['TWILIO_TOKEN'].try(:dup)
-raise "TWILIO_NUMBER not set" unless twilio_number = ENV['TWILIO_NUMBER'].try(:dup)
-raise "TWILIO_TEST_SID not set" unless twilio_test_username = ENV['TWILIO_TEST_SID'].try(:dup)
-raise "TWILIO_TEST_TOKEN not set" unless twilio_test_password = ENV['TWILIO_TEST_TOKEN'].try(:dup)
+raise "twilio_sid not set" unless twilio_username = Conf.twilio_sid
+raise "twilio_token not set" unless twilio_password = Conf.twilio_token
+raise "twilio_number not set" unless twilio_number = Conf.twilio_number
+raise "twilio_test_sid not set" unless twilio_test_username = Conf.twilio_test_sid
+raise "twilio_test_token not set" unless twilio_test_password = Conf.twilio_test_token
 
 twilio_sms_sender = SmsVendor.find_or_initialize_by(name: 'Twilio Sender')
 twilio_sms_sender.update_attributes(
@@ -47,21 +47,21 @@ twilio_voice_test_sender.update_attributes(
 
 sms_loopback = SmsVendor.find_or_initialize_by(name: 'Loopback SMS Sender')
 sms_loopback.update_attributes(
-                                                 worker: 'LoopbackSmsWorker',
-                                                 username: 'sms_loopback_username',
-                                                 password: 'dont care',
-                                                 from: '+15551112222')
+  worker:   'LoopbackSmsWorker',
+  username: 'sms_loopback_username',
+  password: 'dont care',
+  from:     '+15551112222')
 voice_loopback = VoiceVendor.find_or_initialize_by(name: 'Loopback Voice Sender')
 voice_loopback.update_attributes(
-                                                     worker: 'LoopbackVoiceWorker',
-                                                     username: 'voice_loopback_username',
-                                                     password: 'dont care')
+  worker:   'LoopbackVoiceWorker',
+  username: 'voice_loopback_username',
+  password: 'dont care')
 
 
 odm_sender = EmailVendor.find_or_initialize_by(
-    name: 'TMS Extended Sender')
+  name: 'TMS Extended Sender')
 odm_sender.update_attributes(
-    worker: Odm::TMS_EXTENDED_WORKER)
+  worker: Odm::TMS_EXTENDED_WORKER)
 
 email_loopback = EmailVendor.where(name: 'Email Loopback Sender').first_or_create(worker: 'LoopbackEmailWorker')
 
@@ -79,7 +79,7 @@ if Rails.env.development? || Rails.env.ci?
           puts "** using Twilio and ODM senders for default account **"
           Account.find_or_initialize_by(name: "OMG") do |account|
             account.voice_vendor = twilio_voice_sender
-            account.sms_vendor = twilio_sms_sender
+            account.sms_vendor   = twilio_sms_sender
             account.email_vendor = odm_sender
           end.tap do |a|
             a.from_numbers.build(is_default: true, phone_number: twilio_number) unless a.default_from_number
@@ -96,9 +96,9 @@ if Rails.env.development? || Rails.env.ci?
           end
         end
   omg.from_addresses.build(from_email: 'tms_dev@evotest.govdelivery.com',
-                         errors_to:  'errors@evotest.govdelivery.com',
-                         reply_to:   'reply@evotest.govdelivery.com',
-                         is_default: true) unless omg.default_from_address
+                           errors_to:  'errors@evotest.govdelivery.com',
+                           reply_to:   'reply@evotest.govdelivery.com',
+                           is_default: true) unless omg.default_from_address
 
   # stop requests to this account will spray out to DCM accounts ACME and VANDELAY
   omg.dcm_account_codes = Set.new(['ACME', 'VANDELAY'])
@@ -107,24 +107,24 @@ if Rails.env.development? || Rails.env.ci?
 
   # SERVICES FOO => POST to http://localhost/forward
   kw = omg.keywords.with_name('SERVICES').first_or_create!(name: 'SERVICES')
-  kw.create_command!(params: CommandParameters.new(
-    username: "example@evotest.govdelivery.com",
-    password: "password",
-    url: "http://localhost/forward",
-    http_method: "POST",
-    sms_body_param_name: "sms_body1"), command_type: :forward) unless kw.commands.any?
+  kw.create_command!(params:                                            CommandParameters.new(
+                       username:            "example@evotest.govdelivery.com",
+                       password:            "password",
+                       url:                 "http://localhost/forward",
+                       http_method:         "POST",
+                       sms_body_param_name: "sms_body1"), command_type: :forward) unless kw.commands.any?
 
   # SUBSCRIBE ANTHRAX => evolution API request to localhost:3001
   kw = omg.keywords.with_name('SUBSCRIBE').first_or_create!(name: 'SUBSCRIBE')
   kw.create_command!(params: CommandParameters.new(dcm_account_code: "ACME", dcm_topic_codes: ['ANTRHAX']), command_type: :dcm_subscribe) unless kw.commands.any?
 
   # Respond to "DONKEY" with "hee-haw!"
-  kw = omg.keywords.with_name('DONKEY').first_or_create!(name: 'DONKEY', response_text: "hee-haw!")
+  kw   = omg.keywords.with_name('DONKEY').first_or_create!(name: 'DONKEY', response_text: "hee-haw!")
 
   # Make the product user (admin)
   user = omg.users.find_or_initialize_by(email: "product@govdelivery.com") do |u|
     u.password="retek01!"
-    u.admin = true
+    u.admin   = true
   end
   user.save!
   puts "product's token: #{user.authentication_tokens.first.token}"
@@ -132,13 +132,13 @@ if Rails.env.development? || Rails.env.ci?
 elsif Account.count == 0 && User.count ==0
   puts "#{Rails.env} DB looks empty, creating a GovDelivery account."
   account= Account.new(voice_vendor: twilio_voice_sender,
-                           sms_vendor: twilio_sms_sender,
-                           email_vendor: odm_sender,
-                           name: 'GovDelivery')
+                       sms_vendor:   twilio_sms_sender,
+                       email_vendor: odm_sender,
+                       name:         'GovDelivery')
   account.from_addresses.build(is_default: true, from_email: 'info99@service.govdelivery.com')
   account.from_numbers.build(is_default: true, phone_number: twilio_number)
   account.save!
-  user = account.users.create!(email: "product@evotest.govdelivery.com", password: "retek01!")
+  user       = account.users.create!(email: "product@evotest.govdelivery.com", password: "retek01!")
   user.admin = true
   user.save!
 else
