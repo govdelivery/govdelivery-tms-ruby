1d923275c666d492e547ff8b22444794a9dfd6bf
Benjamin Ortega <ben.ortega@gmail.com>
2015-05-05 16:06:25 -0500

    whoops, actually set jakety-jak properties

--- config/initializers/jakety_jak.rb
+++ config/initializers/jakety_jak.rb
@@ -1,1 +1,11 @@
-java.lang.System.getProperties().setProperty("jakety-jak.zk-connect", Conf.analytics_zookeepers.join(','))
+{
+  "jakety-jak.zk-connect"                 => Conf.analytics_zookeepers.join(','),
+  "jakety-jak.streams-per-topic"          => '10',
+  "jakety-jak.max-in-flight-per-stream"   => '128',
+  "jakety-jak.commit-interval-seconds"    => '10',
+  "jakety-jak.commit-after-message-count" => '10000',
+}.each do |property, value|
+  java.lang.System.getProperties().setProperty(property, value)
+end
+# see also: config/application.conf
+
