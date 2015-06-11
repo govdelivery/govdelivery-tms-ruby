047f00c9e9b55bedd2c914ac1d7c56ed29c3b753
Benjamin Ortega <ben.ortega@gmail.com>
2015-06-11 16:39:56 -0500

    fix test to reflect reality

--- spec/workers/analytics/bounce_listener_spec.rb
+++ spec/workers/analytics/bounce_listener_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 describe Analytics::BounceListener do
-  subject {Analytics::BounceListener.new}
+  subject { Analytics::BounceListener.new }
+  let(:message) {
+    {'recipient' => 'fake',
+     'uri'       => 'bounce',
+     'message'   => 'blows'}
+  }
+  let(:java_map) { java.util.HashMap.new(message) }
 
   it 'should have topic' do
     expect(subject.topic).to eq('tms_bounce_channel')
@@ -11,10 +17,7 @@ describe Analytics::BounceListener do
   end
 
   it 'should hand messages off to sidekiq' do
-    message = {'recipient' => 'fake',
-               'uri'       => 'bounce',
-               'message'   => 'blows'}
     Analytics::ProcessBounce.expects(:perform_async).with(message)
-    subject.on_message(message, 1, 1_000)
+    subject.on_message(java_map, 1, 1_000)
   end
 end
