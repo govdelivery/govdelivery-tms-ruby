f788e845e9d2e377b814be28aa7c05e938064461
Alex Ives <alex.ives@govdelivery.com>
2016-03-21 17:14:03 -0500

    Fixed issue in xact voice cucumber config.

--- features/support/config/voice.rb
+++ features/support/config/voice.rb
@@ -1,16 +1,16 @@
 voice = configatron.voice
 
 voice.play_urls = %w(
-  'http://xact-webhook-callbacks.herokuapp.com/voice/first.mp3',
-  'http://xact-webhook-callbacks.herokuapp.com/voice/second.mp3',
-  'http://xact-webhook-callbacks.herokuapp.com/voice/thrid.mp3',
-  'http://xact-webhook-callbacks.herokuapp.com/voice/fourth.mp3',
-  'http://xact-webhook-callbacks.herokuapp.com/voice/fifth.mp3',
-  'http://xact-webhook-callbacks.herokuapp.com/voice/sixth.mp3',
-  'http://xact-webhook-callbacks.herokuapp.com/voice/seventh.mp3',
-  'http://xact-webhook-callbacks.herokuapp.com/voice/eighth.mp3',
-  'http://xact-webhook-callbacks.herokuapp.com/voice/ninth.mp3',
-  'http://xact-webhook-callbacks.herokuapp.com/voice/tenth.mp3'
+  http://xact-webhook-callbacks.herokuapp.com/voice/first.mp3
+  http://xact-webhook-callbacks.herokuapp.com/voice/second.mp3
+  http://xact-webhook-callbacks.herokuapp.com/voice/thrid.mp3
+  http://xact-webhook-callbacks.herokuapp.com/voice/fourth.mp3
+  http://xact-webhook-callbacks.herokuapp.com/voice/fifth.mp3
+  http://xact-webhook-callbacks.herokuapp.com/voice/sixth.mp3
+  http://xact-webhook-callbacks.herokuapp.com/voice/seventh.mp3
+  http://xact-webhook-callbacks.herokuapp.com/voice/eighth.mp3
+  http://xact-webhook-callbacks.herokuapp.com/voice/ninth.mp3
+  http://xact-webhook-callbacks.herokuapp.com/voice/tenth.mp3
 )
 
 voice.recipient.number = '+16124679346' # Probs change this.
