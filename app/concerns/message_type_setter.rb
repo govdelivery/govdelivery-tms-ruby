ac48252ee344318df2834f27dbecf6d9848e7cc2
Chris Thompson <teaforthecat@gmail.com>
2016-05-31 11:40:54 -0500

    [message_type] unset message_type
    with a strange mechanism

--- app/concerns/message_type_setter.rb
+++ app/concerns/message_type_setter.rb
@@ -2,11 +2,13 @@ module MessageTypeSetter
   def self.included(base)
     base.class_eval do
       attr_accessor :message_type_label
-      attr_writer :message_type_code
+      attr_accessor :remove_message_type # the unset tracker, because the message_type_code will cascade
       attr_accessible :message_type_code, :message_type_label
 
       # message types are only auto-created when other things are created
       before_create :auto_create_message_type
+      # this is assuming email messages can't be updated because email_messages shouldn't have their message_types removed
+      before_update :auto_remove_message_type
     end
   end
 
@@ -22,9 +24,18 @@ module MessageTypeSetter
     elsif message_type_label
       errors.add(:message_type_label, "Message type code is required.")
       false
-    elsif self.class.name == 'EmailMessage' && email_template && account #if message_type is undefined for message, inherit from template
+    elsif self.class.name == 'EmailMessage' && email_template && account # if message_type is undefined for message, inherit from template
       self.message_type_id = email_template.try(:message_type).try(:id)
     end
+  end
+
+  def auto_remove_message_type
+    self.message_type_id = nil if remove_message_type
+  end
+
+  def message_type_code=(code)
+    @remove_message_type = true if code.nil?
+    @message_type_code = code
   end
 
   def message_type_code
