ea48b4d702b9756fc67615f8573fac120175145f
Bill Bushey <bill.bushey@govdelivery.com>
2016-06-27 10:02:35 -0500

    Removing the ability to post to from_addresses, which won't work anyway since the API does not support that

--- spec/from_address_spec.rb
+++ spec/from_address_spec.rb
@@ -30,41 +30,6 @@ describe GovDelivery::TMS::FromAddress do
     end
   end
 
-  context 'creating a from address' do
-    let(:client) do
-      double('client')
-    end
-
-    before do
-      @fromaddress = GovDelivery::TMS::FromAddress.new(client, '/from_addresses',            from_email:      'something@evotest.govdelivery.com',
-                                                                                             from_name:       'Something',
-                                                                                             reply_to_email:  'something@evotest.govdelivery.com',
-                                                                                             bounce_email:    'something@evotest.govdelivery.com',
-                                                                                             is_default:      true)
-    end
-
-    it 'should post successfully' do
-      response = {
-        'from_email'      => 'something@evotest.govdelivery.com',
-        'from_name'       => 'Something',
-        'reply_to_email'  => 'something@evotest.govdelivery.com',
-        'bounce_email'    => 'something@evotest.govdelivery.com',
-        'is_default'      => true,
-        'created_at'      => 'sometime',
-        '_links'          => { 'self' => '/from_addresses/1' }
-      }
-      expect(@fromaddress.client).to receive('post').with(@fromaddress).and_return(double('response', status: 201, body: response))
-      @fromaddress.post
-      expect(@fromaddress.from_email).to eq('something@evotest.govdelivery.com')
-      expect(@fromaddress.from_name).to eq('Something')
-      expect(@fromaddress.reply_to_email).to eq('something@evotest.govdelivery.com')
-      expect(@fromaddress.bounce_email).to eq('something@evotest.govdelivery.com')
-      expect(@fromaddress.is_default).to eq(true)
-      expect(@fromaddress.created_at).to eq('sometime')
-      expect(@fromaddress.href).to eq('/from_addresses/1')
-    end
-  end
-
   context 'handling errors' do
     let(:client) do
       double('client')
