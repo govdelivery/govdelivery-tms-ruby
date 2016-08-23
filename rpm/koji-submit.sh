4d3a57a5ba2d050d4a77f2754edf75d92a9507c7
Benjamin Ortega <ben.ortega@gmail.com>
2016-08-23 15:24:07 -0500

    CM-2471 forgot the script

--- rpm/koji-submit.sh
+++ rpm/koji-submit.sh
@@ -1,1 +1,18 @@
 #!/usr/bin/env bash
+if [ -z ${ENV+x} ]; then
+  echo "ENV is not set! Must be \`poc\`, \`qc\`, or \`prod\`" && exit 1;
+fi
+
+if [ -z ${SHA+x} ]; then
+  echo "SHA is not set!" && exit 1;
+fi
+
+if [[ -n ${SCRATCH+x} ]]; then
+  maybe_scratch="--scratch"
+fi
+
+repo="development/xact"
+build_tag="centos6-internal-xact"
+
+cmd="koji build ${maybe_scratch} ${build_tag} 'git+ssh://git@dev-scm.office.gdi/${repo}.git#${SHA}'"
+ssh -qt ${ENV}-koji-service1.ep.gdi sudo -u kojiadmin $cmd
