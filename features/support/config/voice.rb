9df82d0e2f5a41d6d333f78ab794154419fcbb1e
Eric Ebbesen <eebbesen.git@gmail.com>
2016-09-12 15:19:07 -0500

    Need to use the same account when checking the call...

--- features/support/config/voice.rb
+++ features/support/config/voice.rb
@@ -26,6 +26,9 @@ case environment
   when :stage
     voice.send_number = '+16123459610'
     voice.send_number_formatted = '(612) 345-9610'
+    configatron.test_support.twilio.account.sid = 'ACcc41a7e742457806f26d91a1ea19de9f'
+    configatron.test_support.twilio.account.token = '331b3a44b5067a3c02013a6cfaa18b1c'
+
   # need prod account -- see support/config/tms.rb
   # when :prod
   #   voice.send_number = '+16123459610'
