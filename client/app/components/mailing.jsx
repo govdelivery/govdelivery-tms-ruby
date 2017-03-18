3ec8c90f0f39c15ccfecdb86fd280be1f4dc46a7
Serena Wales <serenawr@gmail.com>
2017-03-17 17:47:16 -0700

    Mailing component that doesn't have a mialing in it

---
+++ client/app/components/mailing.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Mailing extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+export default Mailing;
