b6a0509222c441c7895f446374e88f341c67a07b
Serena Wales <serenawr@gmail.com>
2017-03-20 16:51:03 -0700

    First test

--- client/app/components/mailing.jsx
+++ client/app/components/mailing.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Mailing extends React.Component{
   render(){
     return (
-      <div>
+      <div className='mailing'>
         <h1>Hello, world!</h1>
         <h2>It is {new Date().toLocaleTimeString()}.</h2>
       </div>
---
+++ client/test/components/mailing_spec.js
@@ -0,0 +1,17 @@
+import chai, { expect } from 'chai'
+import chaiEnzyme from 'chai-enzyme'
+import { shallow } from 'enzyme'
+
+import React from 'react'
+import Mailing from '../../app/components/mailing'
+
+chai.use(chaiEnzyme())
+
+describe('Mailing', function(){
+
+  it('should initialize Mailing with default props', function() {
+    const wrapper = shallow(<Mailing/>)
+    expect(wrapper).to.have.className('mailing')
+    expect(wrapper.find('h1')).to.have.text('Hello, world!')
+  })
+});
