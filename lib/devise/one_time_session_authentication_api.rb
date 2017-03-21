399d4e93c4e9e64273088fd7bc6dde9037e27409
btilkens <ben.tilkens@govdelivery.com>
2017-03-21 15:36:31 -0500

    some cleanup around request.path

--- lib/devise/one_time_session_authentication_api.rb
+++ lib/devise/one_time_session_authentication_api.rb
@@ -1,12 +1,10 @@
 # An authenticator for users that handles one time session tokens
 class OneTimeSessionAuthenticationApi < Devise::Strategies::Authenticatable
   def valid?
-    # double check that request.url is what we really need here
-    !request.headers['X-AUTH-TOKEN'] && request.url.include?('session')
+    !request.headers['X-AUTH-TOKEN'] && request.path == 'session'
   end
 
   def authenticate!
-    # looking for the query params value in url ie: /session/new?token=1234
     u = OneTimeSessionToken.user_for(params[:token])
     u.nil ? fail! : success!(u)
   end
