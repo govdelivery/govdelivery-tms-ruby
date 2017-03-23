2135744bcbc5d64f6e9dce229ba88c9c72c1b6f2
btilkens <ben.tilkens@govdelivery.com>
2017-03-23 13:37:00 -0500

    XACT-1026 logic around valid? adjusted again

--- lib/devise/one_time_session_authentication_api.rb
+++ lib/devise/one_time_session_authentication_api.rb
@@ -1,12 +1,12 @@
 # An authenticator for users that handles one time session tokens
 class OneTimeSessionAuthenticationApi < Devise::Strategies::Authenticatable
   def valid?
-    !request.headers['X-AUTH-TOKEN'] && request.path == 'session/new'
+    !request.headers['X-AUTH-TOKEN']
   end
 
   def authenticate!
     u = OneTimeSessionToken.user_for(params[:token])
-    u.nil ? fail! : success!(u)
+    u.nil? ? fail! : success!(u)
   end
 end
 Warden::Strategies.add(:one_time_session_auth_api, OneTimeSessionAuthenticationApi)
