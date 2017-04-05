b2ab79c6c371f5d7823cfa36e2b5bf95de7be6a3
btilkens <ben.tilkens@govdelivery.com>
2017-04-05 09:41:30 -0500

    get farts working

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,11 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
 Xact::Application.config.session_store :active_record_store, {
-  :key => '_session_id',
+  :key => '_farts',
   :secure => !['test', 'development'].include?(Rails.env),
   :expire_after =>  2.hours
 }
-
+binding.pry
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
