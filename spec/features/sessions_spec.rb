6edbd674781f39c33ce6668e3121230c26c2690f
Eric Ebbesen <eebbesen.git@gmail.com>
2017-04-10 11:07:25 -0500

    Adding functional rspec test

--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -1,43 +1,52 @@
 require 'airborne'
 require 'rails_helper'
 
-describe 'One-time session' do
+describe 'One-time session', if: ENV['XACT_ENV'] do
   EnvConfig = Struct.new(:auth_token, :host)
 
   def test_env
-    ENV['XACT_ENV'] || Rails.env
+    ENV['XACT_ENV']
   end
 
   def env_config
-    if ['test', 'development'].include? test_env
+    @env_config ||= build_env_config
+  end
+
+  def build_env_config
+    case test_env
+    when 'development'
       EnvConfig.new 'xta4wdrqpACsxFckXpWM784cWyrU96P2', 'http://localhost:3000'
-    elsif 'qc' == test_env
+    when 'qc'
       # pigeon@govdelivery.com
       EnvConfig.new 'hyArYV1PUMP5WAZZqgWeBfLPg7M82pRk', 'https://qc-tms.govdelivery.com'
     else
-      raise "Environment #{test_env} not configured in sessions_spec.rb"
+      raise "ENV['XACT_ENV'] #{test_env} not configured for sessions_spec"
     end
   end
 
+  before(:each) { FakeWeb.allow_net_connect = true }
+  after(:each) { FakeWeb.allow_net_connect = false }
+
   context 'valid token' do
-    before(:each) { FakeWeb.allow_net_connect = true }
     let!(:token) do
       get "#{env_config.host}/user/login", { 'X-AUTH-TOKEN' => env_config.auth_token }
       json = JSON.parse(response.body)
       json['_links']['session'].split('token=')[1]
     end
     scenario 'gets proper cookie' do
-      begin
+      post "#{env_config.host}/session/new", { 'token' => "#{token}" }
 
-        post "#{env_config.host}/session/new", { 'token' => "#{token}" }
-
-        cookie = response.headers[:set_cookie].first
-        expect(cookie).to include '_xact_session='
-        expect(cookie).to include 'HttpOnly'
-        expect(cookie).to include 'secure' unless Rails.env.test? || Rails.env.development?
-      ensure
-        FakeWeb.allow_net_connect = false
-      end
+      cookie = response.headers[:set_cookie].first
+      expect(cookie).to include '_xact_session='
+      expect(cookie).to include 'HttpOnly'
+      expect(cookie).to include 'secure' unless Rails.env.test? || Rails.env.development?
     end
   end
-end 
+  context 'invalid token' do
+    scenario 'gets proper cookie' do
+      post "#{env_config.host}/session/new", { 'token' => 'abcd1234' }
+
+      expect(response).to eq('{"error":"Failed to Login"}')
+    end
+  end
+end
