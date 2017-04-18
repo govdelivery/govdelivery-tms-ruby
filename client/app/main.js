414a72e2173d2beac725652e9e33f7e7fdcccbff
Alex Ives <alex@ives.mn>
2017-04-18 13:11:53 -0400

    XACT-1090: Add menu

--- client/app/main.js
+++ client/app/main.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import * as fetch from './actions/fetches'
 import { store, history } from './store/store'
 import { ConnectedRouter } from 'react-router-redux'
+import { Link } from 'react-router-dom'
 
 // Routing
 import { Route } from 'react-router'
@@ -17,9 +18,19 @@ require('../styles/main.scss')
 ReactDom.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <div className="routes">
-        <Route exact path="/" component={App}/>
-        <Route path="/settings" component={Settings}/>
+      <div className="primary">
+        <div className="sidebar">
+          <div className="sr-primary-nav">
+            <nav role="navigation" aria-label="Main Navigation">
+              <Link to="/">Home</Link>
+              <Link to="/settings">Settings</Link>
+            </nav>
+          </div>
+        </div>
+        <div className="routes">
+          <Route exact path="/" component={App}/>
+          <Route path="/settings" component={Settings}/>
+        </div>
       </div>
     </ConnectedRouter>
   </Provider>,
