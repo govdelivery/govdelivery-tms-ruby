4b33fb59759290e909a1a8330c238fa22e0b5ca9
btilkens <ben.tilkens@govdelivery.com>
2017-04-20 09:38:25 -0500

    XACT-1019 - settings page api callout and styles

--- client/app/components/settings.jsx
+++ client/app/components/settings.jsx
@@ -1,9 +1,35 @@
 // modules/settings.js
 import React from 'react'
 
+const api_callout =
+<div>
+  <div>
+    <div className="sr-align-left sr-card-api-callout">
+      <i className="icon-life_bouy-float-right huge"></i>
+      <h3>
+        Get help with our API
+      </h3>
+      <p>
+        View the <a href="www.google.com">getting started guide</a> or <a href="http://developer.govdelivery.com/api/tms/">visit our developer docs</a>
+      </p>
+    </div>
+  </div>
+  <div className="sr-card-warning">
+    <div className="sr-card-content">
+      <h3>
+        Endpoint URL
+      </h3>
+
+      <p>
+        tms.govdelivery.com
+      </p>
+    </div>
+  </div>
+</div>;
+
 class Settings extends React.Component{
   render() {
-    return <div>settings</div>
+    return api_callout
   }
 }
 Settings.displayName = 'Settings Page'
