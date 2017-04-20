164c93205df19af3f0de2b8ba183ab7b296fbbc5
btilkens <ben.tilkens@govdelivery.com>
2017-04-20 11:11:22 -0500

    XACT-1019 - static content settings page unit tests

--- client/app/components/settings.jsx
+++ client/app/components/settings.jsx
@@ -1,35 +1,34 @@
 // modules/settings.js
 import React from 'react'
 
-const api_callout =
-<div>
-  <div>
-    <div className="sr-align-left sr-card-api-callout">
-      <i className="icon-life_bouy-float-right huge"></i>
-      <h3>
-        Get help with our API
-      </h3>
-      <p>
-        View the <a href="www.google.com">getting started guide</a> or <a href="http://developer.govdelivery.com/api/tms/">visit our developer docs</a>
-      </p>
-    </div>
-  </div>
-  <div className="sr-card-warning">
-    <div className="sr-card-content">
-      <h3>
-        Endpoint URL
-      </h3>
-
-      <p>
-        tms.govdelivery.com
-      </p>
-    </div>
-  </div>
-</div>;
-
 class Settings extends React.Component{
   render() {
-    return api_callout
+    return (
+      <div>
+        <div>
+          <div className="sr-align-left sr-card-api-callout">
+            <i className="icon-life_bouy-float-right huge"></i>
+            <h3 id='get_help'>
+              Get help with our API
+            </h3>
+            <p id='getting_started'>
+              View the <a id='getting_started_link' href="www.google.com">getting started guide</a> or <a id='api_docs_link' href="http://developer.govdelivery.com/api/tms/">visit our developer docs</a>
+            </p>
+          </div>
+        </div>
+        <div className="sr-card-warning">
+          <div className="sr-card-content">
+            <h3>
+              Endpoint URL
+            </h3>
+
+            <p>
+              tms.govdelivery.com
+            </p>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 Settings.displayName = 'Settings Page'
--- client/test/components/settings_spec.js
+++ client/test/components/settings_spec.js
@@ -10,6 +10,17 @@ chai.use(chaiEnzyme())
 describe('Setting', () => {
   it('should initialize Settings', function() {
     const wrapper = shallow(<Setting/>)
-    expect(wrapper.find('div')).to.have.text('settings')
+    expect(wrapper.find('div.sr-align-left > h3')).to.have.text('Get help with our API')
+    expect(wrapper.find('div.sr-align-left > i')).to.have.className('icon-life_bouy-float-right')
+    expect(wrapper.find('div.sr-align-left > p > a#getting_started_link')).to.have.text('getting started guide')
+    expect(wrapper.find('div.sr-align-left > p > a#getting_started_link')).to.have.attr('href')
+    expect(wrapper.find('div.sr-align-left > p > a#api_docs_link')).to.have.text('visit our developer docs')
+    expect(wrapper.find('div.sr-align-left > p > a#api_docs_link')).to.have.attr('href')
+
+    expect(wrapper.find('div.sr-card-content > h3')).to.have.text('Endpoint URL')
+  })
+
+  it('should render an endpoint url based on current environment', function() {
+    // todo
   })
 })
