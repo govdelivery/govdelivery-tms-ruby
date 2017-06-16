b7eed2d599022e3a4fb6e59db1f08ed5384c2b8b
Matt Decuir <matt.decuir@govdelivery.com>
2017-06-16 16:11:33 -0500

    Add missing tests to client sms gem spec

--- spec/email_messages_spec.rb
+++ spec/email_messages_spec.rb
@@ -24,11 +24,11 @@ describe GovDelivery::TMS::EmailMessages do
     end
     
     it 'should GET ok with parameters' do      
-        params = {page_size: 2, sort_by: 'created_by', sort_order: 'foobar'}
-        expect(@messages.client).to receive(:get).with(href, params).and_return(double('response', body: messages[1..-1], status: 200, headers: { 'link' => "</messages/email/page/2>; rel=\"next\",</messages/email/page/11>; rel=\"last\"" }))
-        
-        @messages.get(params)
-        expect(@messages.collection.length).to eq(2)
-      end
+      params = {page_size: 2, sort_by: 'created_by', sort_order: 'foobar'}
+      expect(@messages.client).to receive(:get).with(href, params).and_return(double('response', body: messages[1..-1], status: 200, headers: { 'link' => "</messages/email/page/2>; rel=\"next\",</messages/email/page/11>; rel=\"last\"" }))
+
+      @messages.get(params)
+      expect(@messages.collection.length).to eq(2)
     end
+  end
 end
